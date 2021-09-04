aeb72edc8d3c0f0ca3facddea1b652f4cf14ae59
DemonRed <demonred8@gmail.com>
2021-09-04 14:44:35 +0300

    Removed not needed code, added site Components, React DOM, base Route switches

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header/Header/Header";
+import Aside from "./Components/Aside/Aside";
+import Content from "./Components/Content/Content";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
+  //TODO make different pages, load different components!!!
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Header />
+          <Aside />
+          <Content />
+        </Route>
+        <Route exact path="/login">
+          <Content header_state={true}/>
+        </Route>
+        <Route exact path="/register">
+          <Content header_state={true}/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
