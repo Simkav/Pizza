3139c6e57b706a132cc6f238e909872baad4e91b
DemonRed <demonred8@gmail.com>
2021-09-04 14:43:02 +0300

    Added Aside component

---
+++ client/src/Components/Aside/Aside.js
@@ -0,0 +1,50 @@
+import React from "react";
+import cl from "./Aside.module.css";
+import { useSelector } from "react-redux";
+import { FaQuestion } from "react-icons/fa";
+
+export default function Aside() {
+  const hamburgerMenuState = useSelector((state) => state);
+
+  const asideClasses = [cl.aside_container];
+
+  if (hamburgerMenuState) {
+    asideClasses.push(cl.aside_active);
+  }
+
+  const menu_items_text = [
+    "SALE",
+    "Пицца",
+    "Суши и роллы",
+    "Супы",
+    "Паста и лапша",
+    "Гриль и закуски",
+    "Салаты",
+    "Десерты",
+    "Напитки",
+    "Бизнес-ланчи",
+    "Сигареты",
+  ];
+
+  return (
+    <aside className={asideClasses.join(" ")}>
+      <ul className={cl.main_navigation_menu}>
+        {menu_items_text.map((item) => {
+          return (
+            <li key={item} className={cl.main_navigation_menu_item}>
+              <a href="/login" className={cl.main_navigation_menu_item_link}>
+                <FaQuestion
+                  className={cl.main_navigation_menu_item_text}
+                ></FaQuestion>
+                {item}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className={cl.aside_copyright_container}>
+        <span>© «Mister Cat» является зарегистрированной торговой маркой</span>
+      </div>
+    </aside>
+  );
+}
