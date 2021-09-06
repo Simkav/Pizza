e24de098b523af434ed75e72f57fe091e5cf9b47
DemonRed <demonred8@gmail.com>
2021-09-06 20:20:24 +0300

    init Register page

---
+++ client/src/Pages/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Content from "../Components/Content/Content";
+
+function Register() {
+  return (
+    <div>
+      <Content header_aside_disabled={true} />
+    </div>
+  );
+}
+
+export default Register;
