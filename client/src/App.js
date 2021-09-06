8e8f58b1ffc606ab8a5c937de52e1afad01e226d
DemonRed <demonred8@gmail.com>
2021-09-07 02:32:17 +0300

    Remove not needed code

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import Header from "./Components/Header/Header/Header";
-import Aside from "./Components/Aside/Aside";
-import Content from "./Components/Content/Content";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 
 function App() {
-  //TODO make different pages, load different components!!!
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/register">
-          <Register />
-        </Route>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
         <Redirect to={"/"} />
       </Switch>
     </BrowserRouter>
