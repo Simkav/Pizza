58a2a5c86de929b1acdc7dc9ecd7ea07450562e3
DemonRed <demonred8@gmail.com>
2021-09-07 00:52:14 +0300

    Added Register page code

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,11 +1,75 @@
 import React from "react";
 import Content from "../Components/Content/Content";
+import cl from "./../Styles/Login.module.css";
+import { Link } from "react-router-dom";
 
 function Register() {
+  const email = React.useRef(null);
+  const pass = React.useRef(null);
+  const passCheck = React.useRef(null);
+
   return (
-    <div>
-      <Content header_aside_disabled={true} />
-    </div>
+    <Content header_aside_disabled={true}>
+      <div className={cl.signup_header}>
+        <Link to={"/"} className={cl.site_logo_container}>
+          <img
+            className={cl.site_logo}
+            src="/images/site-logo2.png"
+            alt="Site Logo"
+          ></img>
+        </Link>
+        <div className={cl.login_button_container}>
+          <Link to={"/login"} className={cl.login_button}>
+          <span className={cl.login_button_text}>Войти</span>
+          </Link>
+        </div>
+      </div>
+      <div className={cl.signup_container}>
+        <div className={cl.signup_info}>
+          <h1>Создать аккаунт</h1>
+        </div>
+        <div className={cl.signup_form}>
+          <div className={cl.row}>
+            <div className={cl.field_container}>
+              <input
+                type={"text"}
+                placeholder={"Email адрес"}
+                id={cl.email}
+                className={cl.input}
+                ref={email}
+              ></input>
+            </div>
+          </div>
+          <div className={cl.row}>
+            <div className={cl.field_container}>
+              <input
+                type={"password"}
+                placeholder={"Пароль"}
+                id={cl.password}
+                className={cl.input}
+                ref={pass}
+              ></input>
+            </div>
+          </div>
+          <div className={cl.row}>
+            <div className={cl.field_container}>
+              <input
+                type={"password"}
+                placeholder={"Подтвердите пароль"}
+                id={cl.password}
+                className={cl.input}
+                ref={passCheck}
+              ></input>
+            </div>
+          </div>
+          <div className={cl.row}>
+            <div className={cl.field_container}>
+              <div className={cl.button}>Зарегистрироваться</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Content>
   );
 }
 
