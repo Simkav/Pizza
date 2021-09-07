e085446face04171dc747da1ef2391cc0d8c1ac7
DemonRed <demonred8@gmail.com>
2021-09-07 14:19:03 +0300

    fixed paths

--- client/src/Helpers/HeaderContactPhonesList.js
+++ client/src/Helpers/HeaderContactPhonesList.js
@@ -2,21 +2,21 @@ export const HeaderContactPhonesList = [
   {
     prefix: "044",
     href: "tel:+380442283228",
-    imageSrc: "/icons/standart.svg",
+    imageSrc: "/icons/PhoneOperators/standart.svg",
   },
   {
     prefix: "093",
     href: "tel:+380932283228",
-    imageSrc: "/icons/lifecell.svg",
+    imageSrc: "/icons/PhoneOperators/lifecell.svg",
   },
   {
     prefix: "097",
     href: "tel:+380972283228",
-    imageSrc: "/icons/kyivstar.svg",
+    imageSrc: "/icons/PhoneOperators/kyivstar.svg",
   },
   {
     prefix: "066",
     href: "tel:+380662283228",
-    imageSrc: "/icons/vodafon.svg",
+    imageSrc: "/icons/PhoneOperators/vodafon.svg",
   },
 ];
