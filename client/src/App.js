d4ea7db86644b9fc58cd90daaeb1448b1718e1b2
Ivan Khomenko <simkav2411@gmail.com>
2021-09-07 09:44:29 +0300

    refactor
    using hoc instead of home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-
-function App() {
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Content from './Components/Content/Content'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import WithHeaderFooterAside from './Components/Hoc/WithHeaderFooterAside'
+function App () {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Redirect to={"/"} />
+        <Route exact path='/' component={WithHeaderFooterAside(Content)} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Redirect to={'/'} />
       </Switch>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
