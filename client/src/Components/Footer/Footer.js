4845f1a59932fd02602a422dc91b6623f47d2fac
DemonRed <demonred8@gmail.com>
2021-09-07 13:17:56 +0300

    Added asided state

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
 import cl from "./Footer.module.css";
+import { useSelector } from "react-redux";
 
 export default function Footer() {
+  const hamburgerMenuState = useSelector((state) => state);
+
+  let footerContainerClasses = [cl.footer_container];
+  if (hamburgerMenuState) {
+    footerContainerClasses.push(cl.footer_with_aside);
+  }
+
   return (
-    <div className={cl.footer_container}>
+    <div className={footerContainerClasses.join(" ")}>
       <div className={cl.footer_copyright_container}>
         <span>© «Mister Cat» является зарегистрированной торговой маркой</span>
       </div>
