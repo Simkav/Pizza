e2f208d56b094671f73c64124a424e6bcdc9ae8e
Ivan Khomenko <simkav2411@gmail.com>
2021-09-07 09:43:22 +0300

    remove footer from content

--- client/src/Components/Content/Content.js
+++ client/src/Components/Content/Content.js
@@ -1,28 +1,22 @@
-import React from "react";
-import Footer from "../Footer/Footer";
-import cl from "./Content.module.css";
-import { useSelector } from "react-redux";
+import React from 'react'
+import cl from './Content.module.css'
+import { useSelector } from 'react-redux'
 
-export default function Content(props) {
-  const hamburgerMenuState = useSelector((state) => state);
-  let mainContentContainerClasses;
+export default function Content (props) {
+  const hamburgerMenuState = useSelector(state => state)
+  let mainContentContainerClasses
   if (props.header_aside_disabled) {
-    mainContentContainerClasses = [cl.content_container, cl.content_fullscreen];
+    mainContentContainerClasses = [cl.content_container, cl.content_fullscreen]
   } else {
-    mainContentContainerClasses = [cl.content_container];
+    mainContentContainerClasses = [cl.content_container]
   }
 
   if (hamburgerMenuState) {
-    mainContentContainerClasses.push(cl.content_with_aside);
+    mainContentContainerClasses.push(cl.content_with_aside)
   }
   return (
-    <div className={mainContentContainerClasses.join(" ")}>
+    <div className={mainContentContainerClasses.join(' ')}>
       {props.children}
-      {!props.header_aside_disabled ? (
-        <footer className={cl.content_footer_container}>
-          <Footer />
-        </footer>
-      ) : null}
     </div>
-  );
+  )
 }
