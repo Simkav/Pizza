595cdd74fa18065d162db5bc990b402b8d1daef3
Ivan Khomenko <simkav2411@gmail.com>
2021-09-07 09:43:51 +0300

    add hoc withHeaderFooterAside

---
+++ client/src/Components/Hoc/WithHeaderFooterAside.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header/Header'
+import Footer from '../Footer/Footer'
+import Aside from '../Aside/Aside'
+const WithHeaderFooterAside = Component => props => (
+  <>
+    <Header />
+    <Aside />
+    <Component {...props} />
+    <Footer />
+  </>
+)
+
+export default WithHeaderFooterAside
