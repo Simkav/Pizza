a611965cb21bd993b9d10387dd6994ceeaa14528
DemonRed <demonred8@gmail.com>
2021-09-09 14:18:09 +0300

    Moved PopUpModule directly to /Components folder

--- client/src/Components/PopUpModule/PopUpModule.js
+++ client/src/Components/PopUpModule/PopUpModule.js
@@ -2,13 +2,10 @@ import React from "react";
 import cl from "./PopUpModule.module.css";
 import cn from "classnames";
 
-export default function PopUpModule({ children, visible, setVisible }) {
+export default function PopUpModule({ children, visible }) {
   return (
     <div className={cn(cl.popup, { [cl.active]: visible })}>
-      <div
-        className={cn(cl.popup_container, { [cl.active]: visible })}
-        onClick={() => setVisible(false)}
-      />
+      <div className={cn(cl.popup_container, { [cl.active]: visible })} />
       <div className={cl.popup_children} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
