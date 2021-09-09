7ff5f81f8dff75f8000a38b9d5376f9ee806e658
DemonRed <demonred8@gmail.com>
2021-09-09 17:00:07 +0300

    Removed unnecessary code

--- client/src/Components/Content/Content.js
+++ client/src/Components/Content/Content.js
@@ -1,20 +1,7 @@
 import React from "react";
 import cl from "./Content.module.css";
-import { useSelector } from "react-redux";
 import cn from "classnames";
 
 export default function Content(props) {
-  const hamburgerMenuState = useSelector((state) => state);
-
-  return (
-    <div
-      className={cn(
-        cl.content_container,
-        { [cl.content_fullscreen]: props.header_aside_disabled },
-        { [cl.content_with_aside]: hamburgerMenuState }
-      )}
-    >
-      {props.children}
-    </div>
-  );
+  return <div className={cn(cl.content_container)}>{props.children}</div>;
 }
--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,17 +1,10 @@
 import React from "react";
 import cl from "./Footer.module.css";
-import { useSelector } from "react-redux";
 import cn from "classnames";
 
 export default function Footer() {
-  const hamburgerMenuState = useSelector((state) => state);
-
   return (
-    <div
-      className={cn(cl.footer_container, {
-        [cl.footer_with_aside]: hamburgerMenuState,
-      })}
-    >
+    <div className={cn(cl.footer_container)}>
       <div className={cl.footer_copyright_container}>
         <span>© «Mister Cat» является зарегистрированной торговой маркой</span>
       </div>
