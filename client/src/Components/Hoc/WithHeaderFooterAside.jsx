7d2fa841f65822ee5ba841252944abd8ce5e1e90
DemonRed <demonred8@gmail.com>
2021-09-09 15:19:03 +0300

    Fixed deps path

--- client/src/Components/Hoc/WithHeaderFooterAside.jsx
+++ client/src/Components/Hoc/WithHeaderFooterAside.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../Header/Header/Header'
+import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Aside from '../Aside/Aside'
 const WithHeaderFooterAside = Component => props => (
