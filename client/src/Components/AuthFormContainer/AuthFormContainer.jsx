10fd41078753b5792712ebca1228a73d71950744
DemonRed <demonred8@gmail.com>
2021-09-14 16:41:50 +0300

    Fixed login page title text

--- client/src/Components/AuthFormContainer/AuthFormContainer.jsx
+++ client/src/Components/AuthFormContainer/AuthFormContainer.jsx
@@ -5,7 +5,7 @@ function AuthFormContainer(props) {
     <div className={cl.signup_container}>
       <div className={cl.signup_form_wrapper}>
         <div className={cl.signup_info}>
-          <h1>Войти в аккаунт</h1>
+          <h1>{props.login ? 'Войти в аккаунт' : 'Создать аккаунт'}</h1>
         </div>
         {props.children}
       </div>
