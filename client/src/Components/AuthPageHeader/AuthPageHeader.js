1834e0bbf6c86839857ca7712eac20fa79c10704
DemonRed <demonred8@gmail.com>
2021-09-15 14:01:21 +0300

    Added check isUserAuth

--- client/src/Components/AuthPageHeader/AuthPageHeader.js
+++ client/src/Components/AuthPageHeader/AuthPageHeader.js
@@ -1,8 +1,11 @@
 import React from "react";
 import cl from "./AuthPageHeader.module.css";
 import {Link} from 'react-router-dom'
+import { useSelector } from "react-redux";
 
 function SignUpHeader(props) {
+  const isAuth = useSelector((state) => state.currentUser.isAuth)
+
   return (
     <div className={cl.signup_header}>
       <Link to={"/"} className={cl.site_logo_container}>
@@ -13,8 +16,8 @@ function SignUpHeader(props) {
         ></img>
       </Link>
       <div className={cl.login_button_container}>
-        <Link to={props.login ? "/register" : "/login"} className={cl.login_button}>
-          <span>{props.login ? "Зарегистрироваться" : "Войти"}</span>
+        <Link to={isAuth ? "" : props.login ? "/register" : "/login"} className={cl.login_button}>
+          <span>{isAuth ? "Выйти" : props.login ? "Зарегистрироваться" : "Войти"}</span>
         </Link>
       </div>
     </div>
