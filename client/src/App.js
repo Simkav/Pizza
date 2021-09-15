f125e11d5b76a107e021f8eb0790a53e6130d13a
DemonRed <demonred8@gmail.com>
2021-09-15 13:59:58 +0300

    Added profile route

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
-import Content from './Components/Content/Content'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import WithHeaderFooterAside from './Components/Hoc/WithHeaderFooterAside'
-// For commit
-function App () {
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Content from "./Components/Content/Content";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Profile from "./Pages/Profile";
+import WithHeaderFooterAside from "./Components/Hoc/WithHeaderFooterAside";
+
+
+function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={WithHeaderFooterAside(Content)} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-        <Redirect to={'/'} />
+        <Route exact path="/" component={WithHeaderFooterAside(Content)} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/profile" component={Profile} />
+        <Redirect to={"/"} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
