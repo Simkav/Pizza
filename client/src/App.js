2ec2edbd8c916daba58003a4a557e28372ee297a
DemonRed <demonred8@gmail.com>
2021-09-16 15:45:38 +0300

    Added useLayoutEffect

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,37 @@ import Profile from "./Pages/Profile";
 import WithHeaderFooterAside from "./Components/Hoc/WithHeaderFooterAside";
 import PrivateHoc from "./Components/PrivateHoc/PrivateHoc";
 import OnlyNotAuthorizedUserHoc from "./Components/OnlyNotAuthorizedUserHoc.jsx/OnlyNotAuthorizedUserHoc";
-
+import { useLayoutEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useLayoutEffect(() => {
+    const isAuth = localStorage.getItem("IsAuth");
+    if (isAuth) {
+      dispatch({ type: "AUTHORIZED", payload: true });
+      dispatch({
+        type: "LOGIN_USER",
+        payload: JSON.parse(localStorage.getItem("UserObject")),
+      });
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={WithHeaderFooterAside(Content)} />
-        <Route exact path="/login" component={OnlyNotAuthorizedUserHoc(Login)} />
-        <Route exact path="/register" component={OnlyNotAuthorizedUserHoc(Register)} />
+        <Route
+          exact
+          path="/login"
+          component={OnlyNotAuthorizedUserHoc(Login)}
+        />
+        <Route
+          exact
+          path="/register"
+          component={OnlyNotAuthorizedUserHoc(Register)}
+        />
         <Route exact path="/profile" component={PrivateHoc(Profile)} />
         <Redirect to={"/"} />
       </Switch>
