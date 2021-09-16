b09abc23171ba24530083cc5227758473f83d015
DemonRed <demonred8@gmail.com>
2021-09-16 22:01:53 +0300

    Remake of main route

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import Content from "./Components/Content/Content";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Profile from "./Pages/Profile";
-import WithHeaderFooterAside from "./Components/Hoc/WithHeaderFooterAside";
 import PrivateHoc from "./Components/PrivateHoc/PrivateHoc";
 import OnlyNotAuthorizedUserHoc from "./Components/OnlyNotAuthorizedUserHoc.jsx/OnlyNotAuthorizedUserHoc";
 import { useLayoutEffect } from "react";
 import { useDispatch } from "react-redux";
+import Main from "./Pages/Main";
 
 function App() {
   const dispatch = useDispatch();
@@ -26,7 +25,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={WithHeaderFooterAside(Content)} />
+        <Route exact path="/" component={Main} />
         <Route
           exact
           path="/login"
