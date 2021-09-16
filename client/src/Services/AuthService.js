ca8a7192e52377437ded2c93761b04c26b027e37
DemonRed <demonred8@gmail.com>
2021-09-16 15:46:11 +0300

    Write more data to localStorage

--- client/src/Services/AuthService.js
+++ client/src/Services/AuthService.js
@@ -2,7 +2,8 @@ import $api from "../Axios/Api";
 
 export default async function RegisterService(object) {
   const resp = await $api.post("/users", object);
-  console.log(resp)
+  localStorage.setItem("IsAuth", JSON.stringify(true));
+  localStorage.setItem("UserObject", JSON.stringify(object));
   localStorage.setItem("AccessToken", resp.data.data.access);
   localStorage.setItem("RefreshToken", resp.data.data.refresh);
 
