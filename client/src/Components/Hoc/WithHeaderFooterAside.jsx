16fe5c86900c3e40d697bf56640614639668f5af
DemonRed <demonred8@gmail.com>
2021-09-16 22:02:10 +0300

    Added Content component

--- client/src/Components/Hoc/WithHeaderFooterAside.jsx
+++ client/src/Components/Hoc/WithHeaderFooterAside.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import Aside from '../Aside/Aside'
-const WithHeaderFooterAside = Component => props => (
-  <>
-    <Header />
-    <Aside />
-    <Component {...props} />
-    <Footer />
-  </>
-)
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Aside from "../Aside/Aside";
+import Content from "../Content/Content";
+const WithHeaderFooterAside = (Component) => (props) =>
+  (
+    <>
+      <Header />
+      <Aside />
+      <Content>
+        <Component {...props} />
+      </Content>
+      <Footer />
+    </>
+  );
 
-export default WithHeaderFooterAside
+export default WithHeaderFooterAside;
