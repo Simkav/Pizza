9b16b976f00db1a04c72ecb620c5153d70f5791f
Ivan Khomenko <simkav2411@gmail.com>
2021-09-18 16:08:11 +0300

    add basic error handler

--- server/src/routers/router.js
+++ server/src/routers/router.js
@@ -1,6 +1,10 @@
 const Router = require('express').Router()
 const userRouter = require('./user')
+const errorHandlers = require('../errorHandlers/errorHandler')
+const secondRouter = require('express').Router()
 
 Router.use('/auth', userRouter)
+
+Router.use(errorHandlers.basicHandler)
 
 module.exports = Router
