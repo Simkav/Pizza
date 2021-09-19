f1ae7485d44de4dd4d169290b7e2629fefb5a658
DemonRed <demonred8@gmail.com>
2021-09-19 20:09:49 +0300

    Added actions

--- client/src/Actions/actionCreator.js
+++ client/src/Actions/actionCreator.js
@@ -1,8 +1,27 @@
-import ACTION from './actionTypes'
+import ACTION from "./actionTypes";
 
 export const asideToggle = (data) => {
-    return {
-        type: ACTION.ASIDE_TOGGLE,
-        data: data
-    }
-} 
+  return {
+    type: ACTION.ASIDE_TOGGLE,
+    data: data,
+  };
+};
+
+export const requestAuthRefresh = (refreshToken) => ({
+  type: ACTION.AUTH_REFRESH,
+  data: { refreshToken },
+});
+
+export const authActionLogin = (data) => {
+  return {
+    type: ACTION.AUTH_ACTION_LOGIN,
+    data: data,
+  };
+};
+
+export const authActionRegister = (data) => {
+  return {
+    type: ACTION.AUTH_ACTION_REGISTER,
+    data: data,
+  };
+};
