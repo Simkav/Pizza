90d40a8045e0dbb267e899f65fda1ba9fcf0d519
DemonRed <demonred8@gmail.com>
2021-09-21 16:22:20 +0300

    Refactor

--- client/src/Components/OnlyNotAuthorizedUserHoc.jsx/OnlyNotAuthorizedUserHoc.jsx
+++ client/src/Components/OnlyNotAuthorizedUserHoc.jsx/OnlyNotAuthorizedUserHoc.jsx
@@ -1,9 +1,11 @@
+import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
-const OnlyNotAuthorizedUserHoc = (Component) => (props) => {
-  const isAuth = localStorage.getItem("AccessToken");
-
-  return !isAuth ? <Component /> : <Redirect to={"/"} />;
+const OnlyNotAuthorizedUserHoc = (Component) => {
+  const isAuth = useSelector(({ auth }) => auth).user;
+  return (props) => {
+    return !isAuth ? <Component /> : <Redirect to={"/"} />;
+  };
 };
 
 export default OnlyNotAuthorizedUserHoc;
