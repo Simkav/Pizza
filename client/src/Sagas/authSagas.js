d36a164d1cc3fb950939da40ee16caf32b7775ba
DemonRed <demonred8@gmail.com>
2021-09-21 16:44:41 +0300

    Refactor code

--- client/src/Sagas/authSagas.js
+++ client/src/Sagas/authSagas.js
@@ -1,21 +1,25 @@
 import { put } from "redux-saga/effects";
 import ACTION from "../Actions/actionTypes";
 import * as API from "../Api";
-import history from '../browserHistory'
+import CONSTANTS from "../constants";
 
-export function* registerSaga(action) {
-  yield put(ACTION.AUTH_ACTION_REQUEST);
-
+export function* registerSaga({ history, data }) {
   try {
-      console.log('alllo')
+    yield put({ type: ACTION.AUTH_ACTION_REQUEST });
     const {
       data: {
-        data: { user },
+        data: {
+          user,
+          tokens: { refresh, access },
       },
-    } = yield API.signUp(action.data);
-    history.replace("/");
+      },
+    } = yield API.AuthApi.signUp(data);
     yield put({ type: ACTION.AUTH_ACTION_SUCCESS, user });
+    window.localStorage.setItem(CONSTANTS.REFRESH_TOKEN, refresh);
+    window.localStorage.setItem(CONSTANTS.ACCESS_TOKEN, access);
+    history.push("/");
   } catch (e) {
+    console.log(e);
     yield put({ type: ACTION.AUTH_ACTION_ERROR, error: e.response });
   }
 }
