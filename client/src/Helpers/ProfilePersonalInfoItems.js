1b6bb57540f38113c611ae33fa03aa4aed1b0faf
DemonRed <demonred8@gmail.com>
2021-09-21 16:43:52 +0300

    Remove unnecessary items

--- client/src/Helpers/ProfilePersonalInfoItems.js
+++ client/src/Helpers/ProfilePersonalInfoItems.js
@@ -6,6 +6,4 @@ export const ProfilePersonalInfoItems = [
   },
   { fieldText: "Пароль", type: "password", name: "password" },
   { fieldText: "Email", type: "email", name: "email" },
-  { fieldText: "Имя", type: "text", name: "firstName" },
-  { fieldText: "Фамилия", type: "text", name: "secondName" },
 ];
