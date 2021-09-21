96ed318fa0c8fa3968a12e9aa05ad03f5a7d58ff
DemonRed <demonred8@gmail.com>
2021-09-22 00:56:02 +0300

    Refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,60 @@
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import Profile from './Pages/Profile'
-import OnlyNotAuthorizedUserHoc from './Components/Hoc/OnlyNotAuthorizedUserHoc'
-import PrivateHoc from './Components/Hoc/PrivateHoc'
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import Main from './Pages/Main'
-import constants from './constants'
-import { requestAuthRefresh } from './Actions/actionCreator'
-import LoadSpinner from './Components/LoadSpinner/LoadSpinner'
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Profile from "./Pages/Profile";
+import OnlyNotAuthorizedUserHoc from "./Components/Hoc/OnlyNotAuthorizedUserHoc";
+import PrivateHoc from "./Components/Hoc/PrivateHoc";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Main from "./Pages/Main";
+import constants from "./constants";
+import { requestAuthRefresh } from "./Actions/actionCreator";
+import LoadSpinner from "./Components/LoadSpinner/LoadSpinner";
 
-function App () {
-  const dispatch = useDispatch()
-  const hasUser = useSelector(({ auth: { user } }) => user)
-  const [isShow, setisShow] = useState(false)
+function App() {
+  const dispatch = useDispatch();
+  const hasUser = useSelector(({ auth: { user } }) => user);
+  const [isShow, setisShow] = useState(false);
 
   useEffect(() => {
-    const fn = token => {
-      dispatch(requestAuthRefresh(token))
-    }
-
-    const setShowing = () => {
-      setisShow(true)
-    }
-
-    const refreshToken = localStorage.getItem(constants.REFRESH_TOKEN)
-    if (refreshToken) {
-      if (hasUser) {
-        setShowing()
-      } else {
-        fn(refreshToken)
+    const refreshToken = localStorage.getItem(constants.REFRESH_TOKEN);
+    const fn = () => {
+      if (refreshToken) {
+        if (!hasUser) {
+          dispatch(requestAuthRefresh(refreshToken));
+          return;
+        }
       }
-    } else {
-      setShowing()
-    }
-  }, [hasUser])
+      setisShow(true);
+    };
+    fn();
+  }, [hasUser]);
 
   return isShow ? (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Main} />
+        <Route exact path="/" component={Main} />
         <Route
           exact
-          path='/login'
+          path="/login"
           render={() => <OnlyNotAuthorizedUserHoc Component={Login} />}
         />
         <Route
           exact
-          path='/register'
+          path="/register"
           render={() => <OnlyNotAuthorizedUserHoc Component={Register} />}
         />
         <Route
           exact
-          path='/profile'
+          path="/profile"
           render={() => <PrivateHoc Component={Profile} />}
         />
-        <Redirect to={'/'} />
+        <Redirect to={"/"} />
       </Switch>
     </BrowserRouter>
   ) : (
     <LoadSpinner />
-  )
+  );
 }
 
-export default App
+export default App;
