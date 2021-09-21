5fc1988b3709855f46b3069d1a0746c3a657480d
DemonRed <demonred8@gmail.com>
2021-09-21 16:45:46 +0300

    Add loginSaga

--- client/src/Sagas/rootSaga.js
+++ client/src/Sagas/rootSaga.js
@@ -1,9 +1,10 @@
 import { takeLatest } from "@redux-saga/core/effects";
 import ACTION from "../Actions/actionTypes";
-import { registerSaga } from "./authSagas";
+import { registerSaga, loginSaga } from "./authSagas";
 
 function* rootSaga() {
-  yield takeLatest(ACTION.AUTH_REFRESH, registerSaga);
+  yield takeLatest(ACTION.AUTH_ACTION_REGISTER, registerSaga);
+  yield takeLatest(ACTION.AUTH_ACTION_LOGIN, loginSaga)
 }
 
 export default rootSaga;
