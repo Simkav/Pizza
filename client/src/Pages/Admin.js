cf214a1100c1bc8e7a290a8a004cb97d42f9a57b
DemonRed <demonred8@gmail.com>
2021-09-22 13:06:46 +0300

    Create Admin page

---
+++ client/src/Pages/Admin.js
@@ -0,0 +1,12 @@
+import LoadSpinner from '../Components/LoadSpinner/LoadSpinner'
+
+function Admin() {
+  return (
+    <>
+        <h1>Admin</h1>
+        <LoadSpinner />
+    </>
+  );
+}
+
+export default Admin;
