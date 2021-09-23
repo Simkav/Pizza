f01b13d8b20c5aa2caedabb12642caf7bd81961a
DemonRed <demonred8@gmail.com>
2021-09-23 12:53:58 +0300

    Add AdminPageHeader

--- client/src/Pages/Admin.js
+++ client/src/Pages/Admin.js
@@ -1,10 +1,10 @@
-import LoadSpinner from '../Components/LoadSpinner/LoadSpinner'
+import AdminPageHeader from "../Components/AdminPageHeader/AdminPageHeader";
+import LoadSpinner from "../Components/LoadSpinner/LoadSpinner";
 
 function Admin() {
   return (
     <>
-        <h1>Admin</h1>
-        <LoadSpinner />
+      <AdminPageHeader />
     </>
   );
 }
