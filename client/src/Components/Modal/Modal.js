35c63dbe9c1b7fd0791bd1527c396a90215d9c01
DemonRed <demonred8@gmail.com>
2021-09-23 19:37:17 +0300

    Create Modal component

---
+++ client/src/Components/Modal/Modal.js
@@ -0,0 +1,15 @@
+import cl from "./Modal.module.css";
+import cn from "classnames";
+
+function Modal({ children, visible, setVisible }) {
+  return (
+    <div
+      className={cn(cl.modal_container, { [cl.active]: visible })}
+      onClick={() => setVisible((visible) => !visible)}
+    >
+      <div onClick={(e) => e.stopPropagation()}>{children}</div>
+    </div>
+  );
+}
+
+export default Modal;
