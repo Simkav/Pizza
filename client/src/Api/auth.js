5f91940b36c29c6a413a34aafa3af1f174de8dca
DemonRed <demonred8@gmail.com>
2021-09-23 14:04:15 +0300

    Add interceptors

--- client/src/Api/auth.js
+++ client/src/Api/auth.js
@@ -8,6 +8,15 @@ class AuthApi {
   constructor(client) {
     this.#_client = client;
     this.#_url = "auth/";
+    this.#_accessToken = null;
+
+    this.#_client.interceptors.request.use(this.requestInterceptor, (err) =>
+      Promise.reject(err)
+    );
+    this.#_client.interceptors.response.use(
+      this.responseInterceptor,
+      this.responseInterceptorError
+    );
   }
   signIn = async (data) => this.#_client.post(`${this.#_url}sign-in`, data);
 
@@ -19,6 +28,60 @@ class AuthApi {
     window.localStorage.removeItem(CONSTANTS.REFRESH_TOKEN);
     this.#_accessToken = null;
   };
+
+  requestInterceptor = (config) => {
+    if (this.#_accessToken) {
+      config.headers["Authorization"] = `Bearer ${this.#_accessToken}`;
+    }
+    return config;
+  };
+
+  _saveTokenPair = ({ refresh, access }) => {
+    window.localStorage.setItem(CONSTANTS.REFRESH_TOKEN, refresh);
+    this.#_accessToken = access;
+  };
+
+  responseInterceptor = async (response) => {
+    const {
+      config: { url },
+    } = response;
+    if (url.includes(this._url)) {
+      const {
+        data: {
+          data: { tokenPair },
+        },
+      } = response;
+      this._saveTokenPair(tokenPair);
+    }
+    return response;
+  };
+
+  responseInterceptorError = async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+
+    const refreshToken = window.localStorage.getItem(CONSTANTS.REFRESH_TOKEN);
+    if (status === 419 && refreshToken) {
+      const {
+        data: {
+          data: { tokenPair },
+        },
+      } = await this.refresh({ refreshToken });
+
+      this._saveTokenPair(tokenPair);
+
+      config.headers["Authorization"] = `Bearer ${tokenPair.accessToken}`;
+      return this.#_client(config);
+    }
+
+    if (status === 401 && refreshToken) {
+      this.logout();
+    }
+
+    return Promise.reject(error);
+  }
 }
 
 export default AuthApi;
