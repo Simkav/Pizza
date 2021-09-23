cfbaef17d7e836e64635575890059f0289cb86ff
DemonRed <demonred8@gmail.com>
2021-09-23 19:38:32 +0300

    Create EditProducts page

---
+++ client/src/Pages/EditProducts.js
@@ -0,0 +1,11 @@
+import AdminPageHeader from "../Components/AdminPageHeader/AdminPageHeader";
+
+function EditProducts() {
+  return (
+    <>
+      <AdminPageHeader />
+    </>
+  );
+}
+
+export default EditProducts;
