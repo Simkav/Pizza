33fcf2c14b95cf141005d53a9c5e28e0dfda1b55
DemonRed <demonred8@gmail.com>
2021-09-23 19:39:06 +0300

    Add Ingridients reducer

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
 import HamburgerMenuReducer from "./HamburgerMenuReducer";
+import IngridientsReducer from "./IngridientsReducer";
 
 const appReducer = combineReducers({
   auth: AuthReducer,
   hamburgerMenu: HamburgerMenuReducer,
+  ingridients: IngridientsReducer,
 });
 
 export default appReducer;
