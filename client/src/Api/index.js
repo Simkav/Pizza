b55295be4dcf34899310ce0a3d94ebb244dd4ecc
DemonRed <demonred8@gmail.com>
2021-09-23 19:35:11 +0300

    Add IngridientsCRUD

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -1,8 +1,11 @@
 import axios from "axios";
-import CONSTANTS from '../constants'
-import Auth from './auth'
+import CONSTANTS from "../constants";
+import Auth from "./auth";
+import IngridientsCRUD from "./ingridientsCRUD";
+
 const $api = axios.create({
   baseURL: CONSTANTS.BASE_URL,
 });
 
-export const AuthApi = new Auth($api) 
+export const AuthApi = new Auth($api);
+export const IngridientsCRUDApi = new IngridientsCRUD($api);
