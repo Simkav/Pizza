380f5a55fc91fe15193ce6a8e96797e7c661c8fb
DemonRed <demonred8@gmail.com>
2021-09-23 19:39:22 +0300

    Add ingridients sagas

--- client/src/Sagas/rootSaga.js
+++ client/src/Sagas/rootSaga.js
@@ -1,12 +1,14 @@
 import { takeLatest } from "@redux-saga/core/effects";
 import ACTION from "../Actions/actionTypes";
 import { registerSaga, loginSaga, refreshSaga, logoutSaga } from "./authSagas";
+import { getIngridientsSaga } from "./ingridientsSagas";
 
 function* rootSaga() {
   yield takeLatest(ACTION.AUTH_ACTION_REGISTER, registerSaga);
   yield takeLatest(ACTION.AUTH_ACTION_LOGIN, loginSaga);
   yield takeLatest(ACTION.AUTH_REFRESH, refreshSaga);
   yield takeLatest(ACTION.AUTH_ACTION_CLEAR, logoutSaga);
+  yield takeLatest(ACTION.INGRIDIENTS_ACTION_GET, getIngridientsSaga);
 }
 
 export default rootSaga;
