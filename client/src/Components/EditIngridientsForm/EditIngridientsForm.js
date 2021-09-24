061e352d27f008e3cf146c991e435b7867fcfb16
DemonRed <demonred8@gmail.com>
2021-09-25 00:46:56 +0300

    Refactor

--- client/src/Components/EditIngridientsForm/EditIngridientsForm.js
+++ client/src/Components/EditIngridientsForm/EditIngridientsForm.js
@@ -12,159 +12,114 @@ import {
 import { useFormik } from "formik";
 
 function EditIngridientsForm() {
-  const [isModalOpen, setModalOpen] = useState(false);
-  // TODO Вынести ingridient container в отдельный компонент? IngridientItemForm
-  const [isEdit, setIsEdit] = useState(false);
+  const [isAddModalOpen, setAddModalOpen] = useState(false);
+  const [newIngridient, setNewIngridient] = useState("");
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(ingridientsActionGet());
   }, []);
 
-  const [ingridients, isFetch] = useSelector(({ ingridients }) => [
+  const [ingridients, isFetch, isError] = useSelector(({ ingridients }) => [
     ingridients.ingridients,
     ingridients.isFetching,
+    ingridients.error,
   ]);
 
-  const cancelEdit = (id) => {
-    setIsEdit(false);
-  };
+  const cancelEdit = () => {};
 
-  const applyEdit = (id) => {
-    setIsEdit(false);
-  };
+  const applyEdit = () => {};
 
   const removeIngridient = (id) => {
-    dispatch(ingridientsActionRemove(id));
+    dispatch(ingridientsActionRemove(id, ingridients));
   };
 
-  const IngridientsFormik = useFormik({
-    initialValues: {
-      newIngridient: "",
-      ingridients: ingridients
-        ? ingridients.map((item) => {
-            return { [item.id]: item.name };
-          })
-        : null,
-    },
-    onSubmit: ({ newIngridient }) => {
-      dispatch(ingridientsActionCreate({ name: newIngridient }));
-    setModalOpen(false);
-      IngridientsFormik.resetForm()
-    },
-  });
+  const handleSubmit = () => {
+    dispatch(ingridientsActionCreate({ name: newIngridient }));
+    setAddModalOpen(false);
+    setNewIngridient("");
+  };
 
-  const ingridientsValue = IngridientsFormik.values;
+  const handleCancelAddItem = () => {
+    setAddModalOpen((isMenuOpen) => !isMenuOpen)
+    setNewIngridient("");
+  }
 
   return (
-    <form
-      onSubmit={IngridientsFormik.handleSubmit}
-      className={cl.edit_ingridients_form_container}
-    >
+    <div className={cl.edit_ingridients_form_container}>
       <h1 className={cl.edit_ingridients_form_title}>Ингридиенты</h1>
       <div className={cl.edit_ingridients_form_wrapper}>
         {ingridients
           ? ingridients.map((item, index) => {
-          return (
-            <div key={item.id} className={cl.ingridient_container}>
-              <input
-                    type={"text"}
-                    name={IngridientsFormik.initialValues.ingridients}
-                    onChange={IngridientsFormik.handleChange}
-                    onBlur={IngridientsFormik.handleBlur}
-                disabled={isEdit == item.id ? false : true}
-                className={cl.ingridient_input}
-                    value={ingridients[index].name}
-              />
-              <div className={cl.ingridient_edit_buttons_container}>
-                {isEdit == item.id ? (
-                  <>
+              return (
+                <div key={item.id} className={cl.ingridient_container}>
+                  <span className={cl.ingridient_span}>
+                    {ingridients[index].name}
+                  </span>
+                  <div className={cl.ingridient_edit_buttons_container}>
+                    {
+                      //TODO переделать на изменение в модалке!
+                    }
                     <div
                       className={cl.edit_button_container}
-                      onClick={() => applyEdit(item.id)}
-                    >
-                      <FaCheck className={cl.edit_button} />
-                      <span className={cl.button_tooltip_text}>
-                        Подтвердить
-                      </span>
-                    </div>
-                    <div
-                      className={cl.edit_button_container}
-                      onClick={() => cancelEdit(item.id)}
-                    >
-                      <FaTimes className={cl.edit_button} />
-                          <span className={cl.button_tooltip_text}>
-                            Отменить
-                          </span>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div
-                      className={cl.edit_button_container}
-                      onClick={() => setIsEdit(item.id)}
+                      // onClick={() => setIsEdit(item.id)}
                     >
                       <FaEdit className={cl.edit_button} />
                       <span className={cl.button_tooltip_text}>
                         Редактировать
                       </span>
                     </div>
-                        <div
-                          className={cl.edit_button_container}
-                          onClick={() => removeIngridient(item.id)}
-                        >
+                    <div
+                      className={cl.edit_button_container}
+                      onClick={() => removeIngridient(item.id)}
+                    >
                       <FaTrash className={cl.edit_button} />
-                          <span className={cl.button_tooltip_text}>
-                            Удалить
-                          </span>
+                      <span className={cl.button_tooltip_text}>Удалить</span>
                     </div>
-                  </>
-                )}
-              </div>
-            </div>
-          );
+                  </div>
+                </div>
+              );
             })
           : isFetch
           ? null
           : //TODO Loading spinner
             null}
-        <Modal visible={isModalOpen} setVisible={setModalOpen}>
+        <Modal visible={isAddModalOpen} setVisible={setAddModalOpen}>
           <div className={cl.add_ingridient_window}>
             <h3 className={cl.modal_title}>Добавить ингридиент</h3>
             <input
+              placeholder={"Название ингридиента"}
               type={"text"}
-              name={"newIngridient"}
-              onChange={IngridientsFormik.handleChange}
-              onBlur={IngridientsFormik.handleBlur}
               className={cl.add_ingridient_input}
-              value={ingridientsValue.newIngridient}
+              value={newIngridient}
+              onChange={(e) => setNewIngridient(e.currentTarget.value)}
             />
             <div className={cl.add_window_buttons_container}>
               <button
-                type={"submit"}
+                onClick={() => handleSubmit()}
                 className={cn(cl.add_window_button, cl.apply)}
               >
                 <FaCheck></FaCheck>
               </button>
               <div
                 className={cn(cl.add_window_button, cl.cancel)}
-                onClick={() => setModalOpen((isMenuOpen) => !isMenuOpen)}
+                onClick={() => setAddModalOpen((isMenuOpen) => !isMenuOpen)}
               >
                 <FaTimes></FaTimes>
               </div>
             </div>
           </div>
         </Modal>
-        <div className={cl.add_button_container}>
-          <div
-            className={cl.add_button}
-            onClick={() => setModalOpen((isMenuOpen) => !isMenuOpen)}
-          >
-            Добавить ингридиент
-          </div>
+      </div>
+      <div className={cl.add_button_container}>
+        <div
+          className={cl.add_button}
+          onClick={() => handleCancelAddItem()}
+        >
+          Добавить ингридиент
         </div>
       </div>
-    </form>
+    </div>
   );
 }
 
