4b6eef302960c2f6676b0f9b8e96fc7dba383300
Ivan Khomenko <simkav2411@gmail.com>
2021-09-25 16:20:07 +0300

    refactor server errors

--- server/src/errorHandlers/authErrorHandler.js
+++ server/src/errorHandlers/authErrorHandler.js
@@ -3,7 +3,7 @@ const { TokenExpiredError } = require('jsonwebtoken')
 const authErrorHandler = async (err, req, res, next) => {
   if (err instanceof TokenExpiredError) {
     res.statusCode = 419
-    return res.send('JWT token expired')
+    return res.send({ data: {}, error: 'JWT token expired' })
   }
   next(err)
 }
