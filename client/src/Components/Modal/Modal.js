7f25d825ddc8258a046be42f3d6ca160c7b55d3c
DemonRed <demonred8@gmail.com>
2021-09-26 20:59:02 +0300

    Add optional handleCancel prop

--- client/src/Components/Modal/Modal.js
+++ client/src/Components/Modal/Modal.js
@@ -1,11 +1,11 @@
 import cl from "./Modal.module.css";
 import cn from "classnames";
 
-function Modal({ children, visible, setVisible }) {
+function Modal({ children, visible, setVisible, handleCancel }) {
   return (
     <div
       className={cn(cl.modal_container, { [cl.active]: visible })}
-      onClick={() => setVisible((visible) => !visible)}
+      onClick={() => !handleCancel ? setVisible((visible) => !visible) : handleCancel()}
     >
       <div onClick={(e) => e.stopPropagation()}>{children}</div>
     </div>
