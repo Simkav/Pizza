574399a19cb212fb0faa28a388232d63d87acb66
Ivan Khomenko <simkav2411@gmail.com>
2021-09-27 21:34:10 +0300

    inject morgan and response-time into app

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = require('./routers/router')
+const morgan = require('morgan')
+const responseTime = require('response-time')
 const cors = require('cors')
 const app = express()
 
+app.use(morgan('dev'))
+app.use(responseTime())
 app.use(express.json())
 app.use(cors())
 
