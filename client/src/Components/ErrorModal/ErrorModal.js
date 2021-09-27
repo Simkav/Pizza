790609c55f6503c17b48b14be6d24e8ac3b0e59f
DemonRed <demonred8@gmail.com>
2021-09-27 19:28:01 +0300

    Rename classnames

--- client/src/Components/ErrorModal/ErrorModal.js
+++ client/src/Components/ErrorModal/ErrorModal.js
@@ -6,13 +6,13 @@ import { FaCheck } from "react-icons/fa";
 function ErrorModal({ visible, setVisible, error }) {
   return (
     <Modal visible={visible} setVisible={setVisible}>
-      <div className={cl.add_ingridient_window}>
+      <div className={cl.error_window}>
         <h3 className={cl.modal_title}>Ошибка</h3>
-        <span className={cl.add_ingridient_input}>{error}</span>
-        <div className={cl.add_window_buttons_container}>
+        <span className={cl.error_span}>{error}</span>
+        <div className={cl.error_window_buttons_container}>
           <div
             onClick={() => setVisible((visible) => !visible)}
-            className={cn(cl.add_window_button, cl.apply)}
+            className={cn(cl.error_window_button, cl.apply)}
           >
             <FaCheck></FaCheck>
           </div>
