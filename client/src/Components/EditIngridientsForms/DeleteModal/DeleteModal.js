10016ed2c5b7a61ed84a118a06d10b7d81c7f221
DemonRed <demonred8@gmail.com>
2021-09-27 13:30:23 +0300

    Create DeleteModal

---
+++ client/src/Components/EditIngridientsForms/DeleteModal/DeleteModal.js
@@ -0,0 +1,41 @@
+import Modal from "../../Modal/Modal";
+import cl from "./DeleteModal.module.css";
+import cn from "classnames";
+import { FaTimes, FaCheck } from "react-icons/fa";
+
+function DeleteModal({ visible, setVisible, id, name, handleSubmitRemove }) {
+  const handleSubmit = () => handleSubmitRemove(id);
+
+  const handleCancel = () => setVisible((visible) => !visible);
+
+  return (
+    <Modal
+      visible={visible}
+      setVisible={setVisible}
+      handleCancel={handleCancel}
+    >
+      <div className={cl.add_ingridient_window}>
+        <h3 className={cl.modal_title}>Удалить ингридиент</h3>
+        <span className={cl.add_ingridient_input}>
+          Вы действительно хотите удалить ингридиент: {name}?
+        </span>
+        <div className={cl.add_window_buttons_container}>
+          <button
+            onClick={() => handleSubmit()}
+            className={cn(cl.add_window_button, cl.apply)}
+          >
+            <FaCheck></FaCheck>
+          </button>
+          <div
+            className={cn(cl.add_window_button, cl.cancel)}
+            onClick={() => handleCancel()}
+          >
+            <FaTimes></FaTimes>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default DeleteModal;
