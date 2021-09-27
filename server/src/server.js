a0b5f777d49f7d238ba374a455413aeab56358e1
Ivan Khomenko <simkav2411@gmail.com>
2021-09-27 22:09:59 +0300

    add serving static file

--- server/src/server.js
+++ server/src/server.js
@@ -3,12 +3,15 @@ const router = require('./routers/router')
 const morgan = require('morgan')
 const responseTime = require('response-time')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 
 app.use(morgan('dev'))
 app.use(responseTime())
 app.use(express.json())
 app.use(cors())
+
+app.use('/public', express.static(path.resolve(__dirname, '..', 'public')))
 
 app.use(router)
 
