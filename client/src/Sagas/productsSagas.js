bf9b68bf86d0d0a696fce56d5c0680398a0b303b
DemonRed <demonred8@gmail.com>
2021-09-28 15:16:42 +0300

    Refactor

--- client/src/Sagas/productsSagas.js
+++ client/src/Sagas/productsSagas.js
@@ -1,18 +1,12 @@
-import { put } from "redux-saga/effects";
-import ACTION from "../Actions/actionTypes";
-import * as API from "../Api";
+import { put } from 'redux-saga/effects';
+import ACTION from '../Actions/actionTypes';
+import * as API from '../Api';
 
 export function* getProductsSaga() {
   yield put({ type: ACTION.PRODUCTS_ACTION_GET_REQUEST });
   try {
-    // const {
-    //   data: {
-    //     data: { ingredients },
-    //   },
-    // } = yield API.ProductsCRUDApi.getProducts();
-    const test = yield API.ProductsCRUDApi.getProducts();
-    console.log(test)
-    yield put({ type: ACTION.PRODUCTS_ACTION_GET_SUCCESS, test });
+    const { data } = yield API.ProductsCRUDApi.getProducts();
+    yield put({ type: ACTION.PRODUCTS_ACTION_GET_SUCCESS, products: data });
   } catch (e) {
     console.log(e);
     yield put({
