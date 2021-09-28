80d362df7ae01de4da328918297cf3e32c421916
DemonRed <demonred8@gmail.com>
2021-09-28 19:37:00 +0300

    Add PRODUCTS_ACTION_CLEAR_ERROR

--- client/src/Reducers/ProductsReducer.js
+++ client/src/Reducers/ProductsReducer.js
@@ -99,6 +99,12 @@ export default function (state = initState, action) {
         error: action.error,
       };
     }
+    case ACTION.PRODUCTS_ACTION_CLEAR_ERROR: {
+      return {
+        ...state,
+        error: null,
+      }
+    }
 
     default:
       return state;
