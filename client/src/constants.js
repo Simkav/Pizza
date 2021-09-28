23a5b00e9d19195d9e60729b5aef03869927cf66
DemonRed <demonred8@gmail.com>
2021-09-28 18:47:46 +0300

    Add env usage

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,9 +1,11 @@
-const hostName = 'localhost'
-const serverPort = 3001;
+const { REACT_APP_BACKEND_HOST, REACT_APP_BACKEND_PORT } = process.env;
+
+const hostName = REACT_APP_BACKEND_HOST || 'localhost';
+const serverPort = REACT_APP_BACKEND_PORT || 3001;
 
 export default {
-    ACCESS_TOKEN: 'accessToken',
-    REFRESH_TOKEN: 'refreshToken',
-    BASE_URL: `http://${hostName}:${serverPort}/`,
-    PUBLIC_PATH: `http://${hostName}:${serverPort}/public`   
-}
+  ACCESS_TOKEN: 'accessToken',
+  REFRESH_TOKEN: 'refreshToken',
+  BASE_URL: `http://${hostName}:${serverPort}/`,
+  PUBLIC_PATH: `http://${hostName}:${serverPort}/public`,
+};
