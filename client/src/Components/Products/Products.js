70df73a55856b9c0d801b87982e2d008955a3ae1
DemonRed <demonred8@gmail.com>
2021-09-28 20:14:12 +0300

    fix isErrorModalOpen check

--- client/src/Components/Products/Products.js
+++ client/src/Components/Products/Products.js
@@ -55,7 +55,7 @@ function Products() {
 
   return isProductsFetch || isIngridientsFetch ? (
     <LoadSpinner />
-  ) : isErrorModalOpen ? (
+  ) : isErrorModalOpen.text ? (
     <ErrorModal
       visible={isErrorModalOpen}
       setVisible={setErrorModalOpen}
