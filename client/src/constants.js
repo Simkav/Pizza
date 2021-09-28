6553e12cc6bd578cf141b220c1c177536d213554
Ivan Khomenko <simkav2411@gmail.com>
2021-09-28 21:28:23 +0300

    add some staging scripts

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,11 +1,11 @@
-const { REACT_APP_BACKEND_HOST, REACT_APP_BACKEND_PORT } = process.env;
+const { API_HOSTNAME, API_PORT } = process.env
 
-const hostName = REACT_APP_BACKEND_HOST || 'localhost';
-const serverPort = REACT_APP_BACKEND_PORT || 3001;
+const hostName = API_HOSTNAME || 'localhost'
+const serverPort = API_PORT || 3001
 
 export default {
   ACCESS_TOKEN: 'accessToken',
   REFRESH_TOKEN: 'refreshToken',
   BASE_URL: `http://${hostName}:${serverPort}/`,
-  PUBLIC_PATH: `http://${hostName}:${serverPort}/public`,
-};
+  PUBLIC_PATH: `http://${hostName}:${serverPort}/public`
+}
