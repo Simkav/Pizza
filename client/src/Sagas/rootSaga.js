9a8be318cee28fae374405860974a6d4a390fc1f
DemonRed <demonred8@gmail.com>
2021-09-28 15:11:28 +0300

    Add getProductsSaga

--- client/src/Sagas/rootSaga.js
+++ client/src/Sagas/rootSaga.js
@@ -2,6 +2,7 @@ import { takeLatest } from "@redux-saga/core/effects";
 import ACTION from "../Actions/actionTypes";
 import { registerSaga, loginSaga, refreshSaga, logoutSaga } from "./authSagas";
 import { createIngridientSaga, getIngridientsSaga, removeIngridientSaga, updateIngridientSaga } from "./ingridientsSagas";
+import { getProductsSaga } from "./productsSagas";
 
 function* rootSaga() {
   yield takeLatest(ACTION.AUTH_ACTION_REGISTER, registerSaga);
@@ -12,6 +13,7 @@ function* rootSaga() {
   yield takeLatest(ACTION.INGRIDIENTS_ACTION_REMOVE, removeIngridientSaga);
   yield takeLatest(ACTION.INGRIDIENTS_ACTION_POST, createIngridientSaga);
   yield takeLatest(ACTION.INGRIDIENTS_ACTION_UPDATE, updateIngridientSaga);
+  yield takeLatest(ACTION.PRODUCTS_ACTION_GET, getProductsSaga);
 }
 
 export default rootSaga;
