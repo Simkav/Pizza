c0543f609b26cdd823c6ed4d4b62f75c135f5c33
DemonRed <demonred8@gmail.com>
2021-09-28 15:10:21 +0300

    Add ProductsCRUDApi

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import CONSTANTS from "../constants";
 import Auth from "./auth";
 import IngridientsCRUD from "./ingridientsCRUD";
+import ProductsCRUD from "./productsCRUD";
 
 const $api = axios.create({
   baseURL: CONSTANTS.BASE_URL,
@@ -9,3 +10,4 @@ const $api = axios.create({
 
 export const AuthApi = new Auth($api);
 export const IngridientsCRUDApi = new IngridientsCRUD($api);
+export const ProductsCRUDApi = new ProductsCRUD($api);
