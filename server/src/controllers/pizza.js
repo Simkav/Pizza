fa01d1fc5ef8eff80ed4359ec28bc7203a15818e
Ivan Khomenko <simkav2411@gmail.com>
2021-09-29 14:47:14 +0300

    add cache for pizza

--- server/src/controllers/pizza.js
+++ server/src/controllers/pizza.js
@@ -1,22 +1,9 @@
 const { Pizza, Ingredient } = require('../models')
 
-let Pizzas = null
+let pizzasCache = null
 
-const setPizzas = pizzas => {
-  const formated = pizzas
-    .map(v => v.toJSON())
-    .map(v => {
-      const ingredients = v.Ingredients.reduce((acc, ingredient) => {
-        acc.push(ingredient.id)
-        return acc
-      }, [])
-      return { ...v, Ingredients: ingredients }
-    })
-  Pizzas = formated
-}
-
-const getAll = async (req, res, next) => {
-  if (!Pizzas) {
+const updateCache = async () => {
+  try {
     const pizzas = await Pizza.findAll({
       attributes: { exclude: ['createdAt', 'updatedAt'] },
       include: [
@@ -27,9 +14,27 @@ const getAll = async (req, res, next) => {
         }
       ]
     })
-    setPizzas(pizzas)
+    const formated = pizzas
+      .map(v => v.toJSON())
+      .map(v => {
+        const ingredients = v.Ingredients.reduce((acc, ingredient) => {
+          acc.push(ingredient.id)
+          return acc
+        }, [])
+        return { ...v, Ingredients: ingredients }
+      })
+    pizzasCache = formated
+  } catch (error) {
+    throw new Error('Server Error')
   }
-  res.send(Pizzas)
+}
+
+const getAll = async (req, res, next) => {
+  if (!pizzasCache) {
+    await updateCache()
+  }
+  console.log(pizzasCache)
+  res.send({ data: pizzasCache, error: {} })
 }
 
 module.exports = { getAll }
