8f0e81d285b73eba68ac705f0f14836c4d9981dd
Ivan Khomenko <simkav2411@gmail.com>
2021-09-29 14:48:54 +0300

    refactor saga depends on back changes

--- client/src/Sagas/productsSagas.js
+++ client/src/Sagas/productsSagas.js
@@ -5,7 +5,7 @@ import * as API from '../Api';
 export function* getProductsSaga() {
   yield put({ type: ACTION.PRODUCTS_ACTION_GET_REQUEST });
   try {
-    const { data } = yield API.ProductsCRUDApi.getProducts();
+    const { data: { data } } = yield API.ProductsCRUDApi.getProducts();
     yield put({ type: ACTION.PRODUCTS_ACTION_GET_SUCCESS, products: data });
   } catch (e) {
     console.log(e);
