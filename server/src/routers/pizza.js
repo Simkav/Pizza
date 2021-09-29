05c7fd8752f764332bcb527ec4a4152256c178dc
Ivan Khomenko <simkav2411@gmail.com>
2021-09-30 00:54:59 +0300

    update pizza router

--- server/src/routers/pizza.js
+++ server/src/routers/pizza.js
@@ -1,5 +1,16 @@
 const Router = require('express').Router()
 const PizzaController = require('../controllers/pizza')
+const { pizzasStorage } = require('../services/multer')
+const PizzaMw = require('../middlewares/pizza')
 Router.get('/', PizzaController.getAll)
+
+Router.post('/', pizzasStorage.single('img'), PizzaController.createPizza)
+
+Router.route('/:id')
+  .all(PizzaMw.findPizza)
+  .delete(PizzaController.deletePizza)
+  .patch(PizzaController.deletePizza)
+
+// TODO add logic for update image and ingredients
 
 module.exports = Router
