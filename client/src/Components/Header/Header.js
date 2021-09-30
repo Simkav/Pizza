468a7ba17492c6a74aa25a73c88f450ad6fc82b4
DemonRed <demonred8@gmail.com>
2021-09-30 14:00:30 +0300

    refactor

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,44 +1,21 @@
-import { useState } from "react";
-import cl from "./Header.module.css";
-import PopUpModule from "../PopUpModule/PopUpModule";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  FaBars,
-  FaTimes,
-  FaPhone,
-  FaChevronDown,
-  FaUser,
-  FaShoppingCart,
-} from "react-icons/fa";
-import { bindActionCreators } from "redux";
-import * as ActionCreators from "../../Actions/actionCreator";
-import { Link } from "react-router-dom";
-import { HeaderContactPhonesList } from "../../Helpers/HeaderContactPhonesList";
-import cn from "classnames";
+import cl from './Header.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { asideToggle } from '../../Actions/actionCreator';
+import { Link } from 'react-router-dom';
+import cn from 'classnames';
+import HeaderNavigation from '../HeaderNavigation/HeaderNavigation';
 
 export default function Header() {
   const dispatch = useDispatch();
-  const [contactsPopUp, setContactsPopUp] = useState(false);
-  const [profilePopUp, setProfilePopUp] = useState(false);
 
-  const [isUserAuth, isAdmin] = useSelector(({auth}) => [auth.user, auth.user ? auth.user.isAdmin : false]);
-  
   const hamburgerMenuState = useSelector(
     ({ hamburgerMenu: { isOpened } }) => isOpened
   );
 
-  const { asideToggle, authActionClear } = bindActionCreators(
-    ActionCreators,
-    dispatch
-  );
-
   const hamburgerMenuToggle = () => {
     const toggle = !hamburgerMenuState;
-    asideToggle(toggle);
-  };
-
-  const setLogOut = () => {
-    authActionClear();
+    dispatch(asideToggle(toggle));
   };
 
   return (
@@ -55,7 +32,7 @@ export default function Header() {
           <FaTimes className={cl.hamburger_cross} />
         )}
       </div>
-      <Link to={"/"} className={cl.site_logo_container}>
+      <Link to={'/'} className={cl.site_logo_container}>
         <img
           className={cl.site_logo}
           src="/images/site-logo2.png"
@@ -63,78 +40,7 @@ export default function Header() {
           height="100%"
         />
       </Link>
-      <div className={cl.header_navigation}>
-        <div
-          className={cl.col}
-          onClick={() => setContactsPopUp((contactsPopUp) => !contactsPopUp)}
-        >
-          <div className={cl.contact_phone_container}>
-            <FaPhone />
-            <span className={cl.header_menu_text}>Контакты</span>
-            <FaChevronDown
-              className={cn(cl.arrow_down, {
-                [cl.arrow_active]: contactsPopUp,
-              })}
-            ></FaChevronDown>
-          </div>
-          <PopUpModule visible={contactsPopUp}>
-            {HeaderContactPhonesList.map((item) => {
-              return (
-                <div key={item.prefix} className={cl.contact_phone}>
-                  <a href={item.href} className={cl.phone}>
-                    <img
-                      alt={item.alt}
-                      src={item.imageSrc}
-                      className={cl.phone_operator_icon}
-                    />
-                    {`(${item.prefix}) 228-3-228`}
-                  </a>
-                </div>
-              );
-            })}
-          </PopUpModule>
-        </div>
-        {isUserAuth ? (
-          <div
-            className={cl.col}
-            onClick={() => setProfilePopUp((profilePopUp) => !profilePopUp)}
-          >
-            <div className={cl.contact_phone_container}>
-              <FaUser />
-              <span className={cl.header_menu_text}>Профиль</span>
-              <FaChevronDown
-                className={cn(cl.arrow_down, {
-                  [cl.arrow_active]: profilePopUp,
-                })}
-              ></FaChevronDown>
-            </div>
-            <PopUpModule visible={profilePopUp}>
-              {isAdmin ? (
-                <Link className={cl.profile_link} to={"/admin"}>
-                  Панель администратора
-                </Link>
-              ) : null}
-              <Link className={cl.profile_link} to={"/profile"}>
-                Персональная информация
-              </Link>
-              <div className={cl.profile_link} onClick={setLogOut}>
-                Выйти
-              </div>
-            </PopUpModule>
-          </div>
-        ) : (
-          <Link className={cl.col} to={"/login"}>
-            <div className={cl.contact_phone_container}>
-              <FaUser />
-              <span className={cl.header_menu_text}>Войти</span>
-            </div>
-          </Link>
-        )}
-        <div className={cl.row}>
-          <FaShoppingCart />
-          <span className={cl.header_menu_text}>Корзина</span>
-        </div>
-      </div>
+      <HeaderNavigation />
     </header>
   );
 }
