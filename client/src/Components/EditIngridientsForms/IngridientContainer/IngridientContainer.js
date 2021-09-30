cb20e198aa4ee4d2d37515e28bd871f18220f94e
DemonRed <demonred8@gmail.com>
2021-09-30 13:29:48 +0300

    create IngridientContainer component

---
+++ client/src/Components/EditIngridientsForms/IngridientContainer/IngridientContainer.js
@@ -0,0 +1,32 @@
+import { FaEdit, FaTrash } from "react-icons/fa";
+import cl from './IngridientContainer.module.css'
+
+function IngridientContainer({item, setEditModalOpen, setDeleteModalOpen}) {
+  return (
+      <div key={item.id} className={cl.ingridient_container}>
+        <span className={cl.ingridient_span}>{item.name}</span>
+        <div className={cl.ingridient_edit_buttons_container}>
+          <div
+            className={cl.edit_button_container}
+            onClick={() => {
+              setEditModalOpen(item);
+            }}
+          >
+            <FaEdit className={cl.edit_button} />
+            <span className={cl.button_tooltip_text}>Редактировать</span>
+          </div>
+          <div
+            className={cl.edit_button_container}
+            onClick={() => {
+              setDeleteModalOpen(item);
+            }}
+          >
+            <FaTrash className={cl.edit_button} />
+            <span className={cl.button_tooltip_text}>Удалить</span>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default IngridientContainer;
