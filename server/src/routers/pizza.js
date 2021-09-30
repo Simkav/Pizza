318280d9e2825a47f03c660ba7568ecd342f9eb5
Ivan Khomenko <simkav2411@gmail.com>
2021-09-30 23:51:46 +0300

    complete pizza router

--- server/src/routers/pizza.js
+++ server/src/routers/pizza.js
@@ -1,16 +1,39 @@
-const Router = require('express').Router()
+const PizzaRouter = require('express').Router()
 const PizzaController = require('../controllers/pizza')
 const { pizzasStorage } = require('../services/multer')
 const PizzaMw = require('../middlewares/pizza')
-Router.get('/', PizzaController.getAll)
+const { findIngredients } = require('../middlewares/ingredient')
+const AuthMw = require('../middlewares/auth')
 
-Router.post('/', pizzasStorage.single('img'), PizzaController.createPizza)
+PizzaRouter.get('/', PizzaController.getAll)
 
-Router.route('/:id')
+PizzaRouter.use(AuthMw.checkAccesToken, AuthMw.isAdmin)
+
+PizzaRouter.post(
+  '/',
+  pizzasStorage.single('img'),
+  PizzaMw.parseIngredients,
+  findIngredients,
+  PizzaController.createPizza
+)
+
+PizzaRouter.route('/:id')
   .all(PizzaMw.findPizza)
   .delete(PizzaController.deletePizza)
   .patch(PizzaController.deletePizza)
 
-// TODO add logic for update image and ingredients
+PizzaRouter.post(
+  '/:id/image',
+  PizzaMw.findPizza,
+  pizzasStorage.single('img'),
+  PizzaController.updateImage
+)
 
-module.exports = Router
+PizzaRouter.post(
+  '/:id/ingredients',
+  PizzaMw.findPizza,
+  findIngredients,
+  PizzaController.updateIngredient
+)
+
+module.exports = PizzaRouter
