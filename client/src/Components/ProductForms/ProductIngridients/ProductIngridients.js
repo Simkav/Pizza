b9c554c6118eadf1c343af4ae38613a5915eba57
DemonRed <demonred8@gmail.com>
2021-09-30 14:13:17 +0300

    create ProductIngridients component

---
+++ client/src/Components/ProductForms/ProductIngridients/ProductIngridients.js
@@ -0,0 +1,23 @@
+import cl from './ProductIngridients.module.css'
+
+function ProductIngridients({item, ingridients}) {
+  return (
+    <div className={cl.product_ingridients_container}>
+      <p>
+        {item.Ingredients.map((ingridientID, productIngredientsIndex) => {
+          return ingridients
+            ? ingridients.map((ingridient) => {
+                if (ingridient.id === ingridientID) {
+                  return productIngredientsIndex < item.Ingredients.length - 1
+                    ? `${ingridient.name}, `
+                    : `${ingridient.name}.`;
+                }
+              })
+            : null;
+        })}
+      </p>
+    </div>
+  );
+}
+
+export default ProductIngridients;
