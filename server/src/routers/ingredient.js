b6b327f0fbef3b0c11caaac7f65be8f06d1dc0c9
Ivan Khomenko <simkav2411@gmail.com>
2021-09-30 23:51:30 +0300

    refactor ingredient router

--- server/src/routers/ingredient.js
+++ server/src/routers/ingredient.js
@@ -4,10 +4,10 @@ const ingredientRouter = express.Router()
 const authMiddleware = require('../middlewares/auth')
 const ingredientMiddleware = require('../middlewares/ingredient')
 
-ingredientRouter.get('', ingredientController.getAll)
+ingredientRouter.get('/', ingredientController.getAll)
 
 ingredientRouter.use(authMiddleware.checkAccesToken, authMiddleware.isAdmin)
-ingredientRouter.post('', ingredientController.create)
+ingredientRouter.post('/', ingredientController.create)
 
 ingredientRouter
   .route('/:id')
