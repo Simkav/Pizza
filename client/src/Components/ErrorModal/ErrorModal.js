66eed6ad2c26b48b8ce7969c440dc8cf287a59c7
DemonRed <demonred8@gmail.com>
2021-09-30 14:21:14 +0300

    refactor

--- client/src/Components/ErrorModal/ErrorModal.js
+++ client/src/Components/ErrorModal/ErrorModal.js
@@ -2,22 +2,23 @@ import Modal from '../Modal/Modal';
 import cl from './ErrorModal.module.css';
 import cn from 'classnames';
 import { FaCheck } from 'react-icons/fa';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-function ErrorModal({ visible, setVisible, error, clearError }) {
+function ErrorModal({error, clearError }) {
+  const [isErrorModalOpen, setErrorModalOpen] = useState(true)
   useEffect(() => {
     return () => {
       clearError();
     };
   });
   return (
-    <Modal visible={visible} setVisible={setVisible}>
+    <Modal visible={isErrorModalOpen} setVisible={setErrorModalOpen}>
       <div className={cl.error_window}>
         <h3 className={cl.modal_title}>Ошибка</h3>
         <span className={cl.error_span}>{error}</span>
         <div className={cl.error_window_buttons_container}>
           <div
-            onClick={() => setVisible((visible) => !visible)}
+            onClick={() => setErrorModalOpen((isErrorModalOpen) => !isErrorModalOpen)}
             className={cn(cl.error_window_button, cl.apply)}
           >
             <FaCheck></FaCheck>
