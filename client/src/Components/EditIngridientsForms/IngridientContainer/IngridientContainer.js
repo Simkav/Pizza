4784a1eab903b35753d1bd78e27c67386414565e
DemonRed <demonred8@gmail.com>
2021-10-01 15:16:21 +0300

    refactor

--- client/src/Components/EditIngridientsForms/IngridientContainer/IngridientContainer.js
+++ client/src/Components/EditIngridientsForms/IngridientContainer/IngridientContainer.js
@@ -1,32 +1,34 @@
-import { FaEdit, FaTrash } from "react-icons/fa";
+import { FaEdit, FaTrash } from 'react-icons/fa'
 import cl from './IngridientContainer.module.css'
 
-function IngridientContainer({item, setEditModalOpen, setDeleteModalOpen}) {
+export default function IngridientContainer ({
+  item,
+  setEditModalOpen,
+  setDeleteModalOpen
+}) {
   return (
-      <div className={cl.ingridient_container}>
-        <span className={cl.ingridient_span}>{item.name}</span>
-        <div className={cl.ingridient_edit_buttons_container}>
-          <div
-            className={cl.edit_button_container}
-            onClick={() => {
-              setEditModalOpen(item);
-            }}
-          >
-            <FaEdit className={cl.edit_button} />
-            <span className={cl.button_tooltip_text}>Редактировать</span>
-          </div>
-          <div
-            className={cl.edit_button_container}
-            onClick={() => {
-              setDeleteModalOpen(item);
-            }}
-          >
-            <FaTrash className={cl.edit_button} />
-            <span className={cl.button_tooltip_text}>Удалить</span>
-          </div>
+    <div className={cl.ingridient_container}>
+      <span className={cl.ingridient_span}>{item.name}</span>
+      <div className={cl.ingridient_edit_buttons_container}>
+        <div
+          className={cl.edit_button_container}
+          onClick={() => {
+            setEditModalOpen({ ...item, state: true })
+          }}
+        >
+          <FaEdit className={cl.edit_button} />
+          <span className={cl.button_tooltip_text}>Редактировать</span>
+        </div>
+        <div
+          className={cl.edit_button_container}
+          onClick={() => {
+            setDeleteModalOpen({ ...item, state: true })
+          }}
+        >
+          <FaTrash className={cl.edit_button} />
+          <span className={cl.button_tooltip_text}>Удалить</span>
         </div>
       </div>
-  );
+    </div>
+  )
 }
-
-export default IngridientContainer;
