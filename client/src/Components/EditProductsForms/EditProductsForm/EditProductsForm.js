638b292fd143ad1f5d436cf06b2d8bf8c8f0693f
DemonRed <demonred8@gmail.com>
2021-10-03 16:02:05 +0300

    create EditProductForm component

---
+++ client/src/Components/EditProductsForms/EditProductsForm/EditProductsForm.js
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux'
+import EditProductsList from '../EditProductsList/EditProductsList'
+
+export default function EditProductsForm () {
+  const ingridients = useSelector(({ ingridients }) => ingridients.ingridients)
+  const products = useSelector(({ products }) => products.products)
+  return (
+    <div>
+      <EditProductsList ingridients={ingridients} products={products} />
+    </div>
+  )
+}
