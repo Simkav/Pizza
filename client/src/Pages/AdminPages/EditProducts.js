b0cb9c2319baffaf87f282f728eb5d21084c1fb4
DemonRed <demonred8@gmail.com>
2021-10-03 16:03:49 +0300

    add EditProductsForm

--- client/src/Pages/AdminPages/EditProducts.js
+++ client/src/Pages/AdminPages/EditProducts.js
@@ -1,9 +1,11 @@
 import AdminPageHeader from "../../Components/AdminPageHeader/AdminPageHeader";
+import EditProductsForm from "../../Components/EditProductsForms/EditProductsForm/EditProductsForm";
 
 export default function EditProducts() {
   return (
     <>
       <AdminPageHeader />
+      <EditProductsForm />
     </>
   );
 }
