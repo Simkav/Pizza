cbf0c73c693e5bb3284ff7d6ecdb4f36165eb24b
DemonRed <demonred8@gmail.com>
2021-10-10 19:22:46 +0300

    add edit buttons

--- client/src/Components/EditProductsForms/EditProduct/EditProduct.js
+++ client/src/Components/EditProductsForms/EditProduct/EditProduct.js
@@ -1,9 +1,10 @@
+import { FaEdit, FaTrash } from 'react-icons/fa'
 import ProductFooter from '../../ProductForms/ProductFooter/ProductFooter'
 import ProductImage from '../../ProductForms/ProductImage/ProductImage'
 import ProductIngridients from '../../ProductForms/ProductIngridients/ProductIngridients'
 import cl from './EditProduct.module.css'
 
-export default function EditProduct ({ item, ingridients }) {
+export default function EditProduct ({ item, ingridients, setDeleteModalOpen, setEditModalOpen }) {
   return (
     <li key={item.id} className={cl.product}>
       {
@@ -12,6 +13,26 @@ export default function EditProduct ({ item, ingridients }) {
           <h3>{item.name}</h3>
           <ProductIngridients item={item} ingridients={ingridients} />
           <ProductFooter weight={item.weight} price={item.price} />
+          <div className={cl.product_edit_buttons_container}>
+            <div
+              className={cl.edit_button_container}
+              onClick={() => {
+                setEditModalOpen({ ...item, state: true })
+              }}
+            >
+              <FaEdit className={cl.edit_button} />
+              <span className={cl.button_tooltip_text}>Редактировать</span>
+            </div>
+            <div
+              className={cl.edit_button_container}
+              onClick={() => {
+                setDeleteModalOpen({ id: item.id ,name: item.name, state: true })
+              }}
+            >
+              <FaTrash className={cl.edit_button} />
+              <span className={cl.button_tooltip_text}>Удалить</span>
+            </div>
+          </div>
         </div>
       }
     </li>
