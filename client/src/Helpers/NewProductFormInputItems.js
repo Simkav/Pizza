6c18dfb5356585fb1c74acb992fb45bb6af977fb
DemonRed <demonred8@gmail.com>
2021-10-10 19:17:38 +0300

    create NewProduct form input items helper

---
+++ client/src/Helpers/NewProductFormInputItems.js
@@ -0,0 +1,5 @@
+export const NewProductFormInputItems = [
+  { labelText: 'Название продукта', type: 'text', name: 'name' },
+  { labelText: 'Цена продукта', type: 'number', name: 'price' },
+  { labelText: 'Вес продукта', type: 'number', name: 'weight' },
+]
