f560be3a9ee93be73fd4b9264deb037f6a8f9182
DemonRed <demonred8@gmail.com>
2021-10-12 17:48:01 +0300

    refactor

--- client/src/Components/ProductForms/ProductIngridients/ProductIngridients.js
+++ client/src/Components/ProductForms/ProductIngridients/ProductIngridients.js
@@ -4,15 +4,15 @@ export default function ProductIngridients ({ item, ingridients }) {
   return (
     <div className={cl.product_ingridients_container}>
       <p>
-        {item.Ingredients.map((ingridientID, productIngredientsIndex) => {
-          return ingridients.map(ingridient => {
-            if (ingridient.id === ingridientID) {
-              return productIngredientsIndex < item.Ingredients.length - 1
+        {item.Ingredients.map((ingridientID, productIngredientsIndex) =>
+          ingridients.map(ingridient =>
+            ingridient.id === ingridientID
+              ? productIngredientsIndex < item.Ingredients.length - 1
                 ? `${ingridient.name}, `
                 : `${ingridient.name}.`
-            }
-          })
-        })}
+              : null
+          )
+        )}
       </p>
     </div>
   )
