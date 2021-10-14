5899423d66519cbeaf5e4593d0aed6395e1dbdb9
DemonRed <demonred8@gmail.com>
2021-10-14 17:59:46 +0300

    fix item ingredients prop name

--- client/src/Components/ProductForms/ProductIngridients/ProductIngridients.js
+++ client/src/Components/ProductForms/ProductIngridients/ProductIngridients.js
@@ -4,10 +4,10 @@ export default function ProductIngridients ({ item, ingridients }) {
   return (
     <div className={cl.product_ingridients_container}>
       <p>
-        {item.Ingredients.map((ingridientID, productIngredientsIndex) =>
+        {item.ingredients.map((ingridientID, productIngredientsIndex) =>
           ingridients.map(ingridient =>
             ingridient.id === ingridientID
-              ? productIngredientsIndex < item.Ingredients.length - 1
+              ? productIngredientsIndex < item.ingredients.length - 1
                 ? `${ingridient.name}, `
                 : `${ingridient.name}.`
               : null
