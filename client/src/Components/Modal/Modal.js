61386e565f883b826df996480de6b5aea864029a
DemonRed <demonred8@gmail.com>
2021-10-18 17:55:14 +0300

    refactor Modal

--- client/src/Components/Modal/Modal.js
+++ client/src/Components/Modal/Modal.js
@@ -1,15 +1,29 @@
-import cl from "./Modal.module.css";
+import cl from './Modal.module.css'
 import ReactModal from 'react-modal'
 
-export default function Modal({ children, visible, handleCancel }) {
+export default function Modal ({
+  children,
+  visible,
+  handleClose,
+  handleClosed
+}) {
   return (
     <ReactModal
       isOpen={visible}
-      onRequestClose={() => handleCancel()}
+      onAfterClose={() => (handleClosed ? handleClosed() : null)}
+      onRequestClose={() => handleClose()}
+      on
       className={cl.ReactModal_Content}
       shouldFocusAfterRender={false}
-      overlayClassName={{base: cl.ReactModal_Overlay, afterOpen: cl.ReactModal_Overlay_after_open, beforeClose: cl.ReactModal_Overlay_before_close }}
-    >{children}
+      openTimeoutMS={500}
+      closeTimeoutMS={500}
+      overlayClassName={{
+        base: cl.ReactModal_Overlay,
+        afterOpen: cl.ReactModal_Overlay_after_open,
+        beforeClose: cl.ReactModal_Overlay_before_close
+      }}
+    >
+      {children}
     </ReactModal>
-  );
+  )
 }
