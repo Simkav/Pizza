a0ebecab9bc0429d79c3779995593b7faa85973b
DemonRed <demonred8@gmail.com>
2021-10-19 17:05:05 +0300

    fix

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,7 +3,7 @@ const { REACT_APP_BACKEND_HOST, REACT_APP_BACKEND_PORT } = process.env
 const hostName = REACT_APP_BACKEND_HOST || 'localhost'
 const serverPort = REACT_APP_BACKEND_PORT || 3001
 
-const SUPPORTED_FORMATS = ['image/png', 'image/svg']
+const SUPPORTED_FORMATS = ['image/png', 'image/svg','image/jpeg']
 
 export default {
   SUPPORTED_FORMATS: SUPPORTED_FORMATS,
