9152d7c6644f783cec33546446d4448dbf191b7a
DemonRed <demonred8@gmail.com>
2021-10-19 17:51:46 +0300

    add ingridient name validation

--- client/src/Components/EditIngridientsForms/EditModal/EditModal.js
+++ client/src/Components/EditIngridientsForms/EditModal/EditModal.js
@@ -3,12 +3,16 @@ import cl from './EditModal.module.css'
 import cn from 'classnames'
 import { FaTimes, FaCheck } from 'react-icons/fa'
 import { useLayoutEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { ingridientsActionUpdate } from '../../../Actions/actionCreator'
+import EditIngridientInput from '../EditIngridientInput/EditIngridientInput'
 
 export default function EditModal ({ modalsState, modalsDispatch }) {
   const dispatch = useDispatch()
   const [newName, setNewName] = useState(modalsState.editModal.name)
+  const [isInvalid, setIsInvalid] = useState(false)
+
+  const ingridients = useSelector(({ ingridients }) => ingridients.ingridients)
 
   useLayoutEffect(() => {
     if (modalsState.editModal.state && !newName) {
@@ -26,6 +30,19 @@ export default function EditModal ({ modalsState, modalsDispatch }) {
   const handleClosed = () => {
     modalsDispatch({ type: 'ON_EDIT_MODAL_CLOSED' })
     setNewName('')
+    setIsInvalid(false)
+  }
+
+  const validate = value => {
+    if (!value) {
+      setIsInvalid('Как минимум один символ')
+    }
+    if (ingridients.find(v => v.name === value)) {
+      setIsInvalid('Такой ингридиент уже существует')
+    } else if (value && !ingridients.find(v => v.name === value)) {
+      setIsInvalid(false)
+    }
+    setNewName(value)
   }
 
   return (
@@ -36,16 +53,14 @@ export default function EditModal ({ modalsState, modalsDispatch }) {
     >
       <div className={cl.edit_ingridient_window}>
         <h3 className={cl.modal_title}>Редактировать ингридиент</h3>
-        <input
-          placeholder={'Название ингридиента'}
-          type={'text'}
-          className={cl.edit_ingridient_input}
-          value={newName}
-          onChange={e => setNewName(e.currentTarget.value)}
+        <EditIngridientInput
+          name={newName}
+          isInvalid={isInvalid}
+          validate={validate}
         />
         <div className={cl.edit_window_buttons_container}>
           <button
-            onClick={() => handleSubmit()}
+            onClick={() => !isInvalid ? handleSubmit() : null}
             className={cn(cl.edit_window_button, cl.apply)}
           >
             <FaCheck />
