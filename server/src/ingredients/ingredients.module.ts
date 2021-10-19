483da71624d1543918ebc2d7536d4bd0edf101ff
Ivan Khomenko <simkav2411@gmail.com>
2021-10-19 15:48:36 +0300

    apply parseingredientid mw

--- server/src/ingredients/ingredients.module.ts
+++ server/src/ingredients/ingredients.module.ts
@@ -1,10 +1,11 @@
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { IngredientsController } from './ingredients.controller';
 import { IngredientsService } from './ingredients.service';
 import { Ingredient } from './ingredients.model';
 import { Pizza } from 'src/pizzas/pizza.model';
 import { PizzaIngredients } from 'src/pizzas/pizza-ingredients.model';
+import { parseIngredientId } from 'src/middlewares/parse-ingredient-id.middleware';
 
 @Module({
   imports: [SequelizeModule.forFeature([Pizza, Ingredient, PizzaIngredients])],
@@ -12,4 +13,11 @@ import { PizzaIngredients } from 'src/pizzas/pizza-ingredients.model';
   providers: [IngredientsService],
   exports: [IngredientsService],
 })
-export class IngredientsModule {}
+export class IngredientsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(parseIngredientId)
+      .exclude('ingredients')
+      .forRoutes(IngredientsController);
+  }
+}
