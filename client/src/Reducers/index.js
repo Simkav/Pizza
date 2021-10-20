b5a134d79bbe08580066b3cacfba79b1d13b7d3a
DemonRed <demonred8@gmail.com>
2021-10-20 21:09:37 +0300

    add CartReducer

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -3,12 +3,14 @@ import AuthReducer from "./AuthReducer";
 import HamburgerMenuReducer from "./HamburgerMenuReducer";
 import IngridientsReducer from "./IngridientsReducer";
 import ProductsReducer from "./ProductsReducer";
+import CartReducer from './CartReducer'
 
 const appReducer = combineReducers({
   auth: AuthReducer,
   hamburgerMenu: HamburgerMenuReducer,
   ingridients: IngridientsReducer,
   products: ProductsReducer,
+  cart: CartReducer
 });
 
 export default appReducer;
