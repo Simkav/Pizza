b68ac4b083f1e9484f4cc3d6cdc87cb78c3f9483
DemonRed <demonred8@gmail.com>
2021-10-24 15:51:33 +0300

    refactor Product component

--- client/src/Components/ProductForms/Product/Product.js
+++ client/src/Components/ProductForms/Product/Product.js
@@ -1,21 +1,30 @@
-import cl from './Product.module.css'
-import ProductImage from '../ProductImage/ProductImage'
-import ProductIngridients from '../ProductIngridients/ProductIngridients'
-import ProductFooter from '../ProductFooter/ProductFooter'
-import { useState } from 'react'
+import cl from './Product.module.css';
+import ProductImage from '../ProductImage/ProductImage';
+import ProductIngridients from '../ProductIngridients/ProductIngridients';
+import ProductFooter from '../ProductFooter/ProductFooter';
+import { useState } from 'react';
 
-export default function Product ({ item, ingridients }) {
-  const [isHovered, setHovered] = useState(false)
+export default function Product ({ setIsOrdered, item, ingridients }) {
+  const [isHovered, setHovered] = useState(false);
+
   return (
     <li className={cl.product}>
       {
-        <div className={cl.product_link_container} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(isHovered => !isHovered)}>
+        <div
+          className={cl.product_link_container}
+          onMouseOver={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
           <ProductImage item={item} />
           <h3>{item.name}</h3>
           <ProductIngridients item={item} ingridients={ingridients} />
-          <ProductFooter item={item} isHovered={isHovered} />
+          <ProductFooter
+            isHovered={isHovered}
+            setIsOrdered={setIsOrdered}
+            item={item}
+          />
         </div>
       }
     </li>
-  )
+  );
 }
