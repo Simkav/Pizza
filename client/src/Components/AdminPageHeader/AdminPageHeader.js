a086806cce53653f23ab003571d1133b3a1ec1d8
DemonRed <demonred8@gmail.com>
2021-10-25 16:55:11 +0300

    add useHovered

--- client/src/Components/AdminPageHeader/AdminPageHeader.js
+++ client/src/Components/AdminPageHeader/AdminPageHeader.js
@@ -3,26 +3,31 @@ import cl from './AdminPageHeader.module.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import PopUpModule from '../PopUpModule/PopUpModule';
+import useHovered from '../../Hooks/useHovered';
 
-export default function AdminPageHeader() {
+export default function AdminPageHeader () {
   const [menuPopUp, setMenuPopUp] = useState(false);
+  const [isHovered, menuButtonRef] = useHovered();
 
   return (
     <div className={cl.auth_header}>
       <Link to={'/'} className={cl.site_logo_container}>
         <img
           className={cl.site_logo}
-          src="/images/site-logo2.png"
-          alt="Site Logo"
+          src='/images/site-logo2.png'
+          alt='Site Logo'
         ></img>
       </Link>
       <div className={cl.menu_button_container}>
         <div
           className={cl.menu_button}
+          tabIndex={0}
+          ref={menuButtonRef}
           onClick={() => setMenuPopUp((menuPopUp) => !menuPopUp)}
+          onBlur={() => setMenuPopUp(false)}
         >
           Меню
-          <PopUpModule visible={menuPopUp}>
+          <PopUpModule visible={menuPopUp} hovered={isHovered}>
             <Link className={cl.menu_link} to={'/edit_products'}>
               Редактировать продукты
             </Link>
