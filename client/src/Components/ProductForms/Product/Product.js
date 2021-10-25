62a313155317e2722ea8e0cbbd39b4aa46ecb96b
DemonRed <demonred8@gmail.com>
2021-10-25 14:39:26 +0300

    add useHovered hook

--- client/src/Components/ProductForms/Product/Product.js
+++ client/src/Components/ProductForms/Product/Product.js
@@ -2,19 +2,15 @@ import cl from './Product.module.css';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductIngridients from '../ProductIngridients/ProductIngridients';
 import ProductFooter from '../ProductFooter/ProductFooter';
-import { useState } from 'react';
+import useHovered from '../../../Hooks/useHovered';
 
-export default function Product({ setIsOrdered, item, ingridients }) {
-  const [isHovered, setHovered] = useState(false);
-
+export default function Product ({ setIsOrdered, item, ingridients }) {
+  const [isHovered, hoverRef] = useHovered();
+  
   return (
     <li className={cl.product}>
       {
-        <div
-          className={cl.product_link_container}
-          onMouseOver={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-        >
+        <div ref={hoverRef} className={cl.product_link_container}>
           <ProductImage item={item} />
           <h3>{item.name}</h3>
           <ProductIngridients item={item} ingridients={ingridients} />
