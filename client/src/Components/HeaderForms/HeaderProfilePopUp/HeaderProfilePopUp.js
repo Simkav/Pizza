a52fc99d74e736033b7d791d512ca0131d0fc739
DemonRed <demonred8@gmail.com>
2021-10-25 20:00:18 +0300

    add usePopUp hook

--- client/src/Components/HeaderForms/HeaderProfilePopUp/HeaderProfilePopUp.js
+++ client/src/Components/HeaderForms/HeaderProfilePopUp/HeaderProfilePopUp.js
@@ -1,17 +1,15 @@
 import cl from './HeaderProfilePopUp.module.css';
 import cn from 'classnames';
-import { useState } from 'react';
 import { FaChevronDown, FaUser } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { authActionClear } from '../../../Actions/actionCreator';
 import PopUpModule from '../../PopUpModule/PopUpModule';
-import useHovered from '../../../Hooks/useHovered';
+import usePopUp from '../../../Hooks/usePopUp';
 
 export default function HeaderProfilePopUp () {
   const dispatch = useDispatch();
-  const [profilePopUp, setProfilePopUp] = useState(false);
-  const [isHovered, menuButtonRef] = useHovered();
+  const [isHovered, profilePopUp, menuButtonRef] = usePopUp();
 
   const [isUserAuth, isAdmin] = useSelector(({ auth }) => [
     auth.user,
@@ -22,43 +20,41 @@ export default function HeaderProfilePopUp () {
     dispatch(authActionClear());
   };
 
-  return isUserAuth ? (
-    <div
-      tabIndex={0}
-      ref={menuButtonRef}
-      className={cl.col}
-      onClick={() => setProfilePopUp((profilePopUp) => !profilePopUp)}
-      onBlur={() => setProfilePopUp(false)}
-    >
-      <div className={cl.button_container}>
-        <FaUser />
-        <span className={cl.header_menu_text}>Профиль</span>
-        <FaChevronDown
-          className={cn(cl.arrow_down, {
-            [cl.arrow_active]: profilePopUp || isHovered,
-          })}
-        ></FaChevronDown>
-      </div>
-      <PopUpModule visible={profilePopUp} hovered={isHovered}>
-        {isAdmin ? (
-          <Link className={cl.profile_link} to={'/admin'}>
-            Панель администратора
-          </Link>
-        ) : null}
-        <Link className={cl.profile_link} to={'/profile'}>
-          Персональная информация
+  return (
+    <div ref={menuButtonRef} className={cl.col}>
+      {isUserAuth ? (
+        <>
+          <div className={cl.button_container}>
+            <FaUser />
+            <span className={cl.header_menu_text}>Профиль</span>
+            <FaChevronDown
+              className={cn(cl.arrow_down, {
+                [cl.arrow_active]: profilePopUp || isHovered,
+              })}
+            ></FaChevronDown>
+          </div>
+          <PopUpModule visible={profilePopUp} hovered={isHovered}>
+            {isAdmin ? (
+              <Link className={cl.profile_link} to={'/admin'}>
+                Панель администратора
+              </Link>
+            ) : null}
+            <Link className={cl.profile_link} to={'/profile'}>
+              Персональная информация
+            </Link>
+            <div className={cl.profile_link} onClick={setLogOut}>
+              Выйти
+            </div>
+          </PopUpModule>
+        </>
+      ) : (
+        <Link className={cl.col} to={'/login'}>
+          <div className={cl.button_container}>
+            <FaUser />
+            <span className={cl.header_menu_text}>Войти</span>
+          </div>
         </Link>
-        <div className={cl.profile_link} onClick={setLogOut}>
-          Выйти
-        </div>
-      </PopUpModule>
+      )}
     </div>
-  ) : (
-    <Link className={cl.col} to={'/login'}>
-      <div className={cl.button_container}>
-        <FaUser />
-        <span className={cl.header_menu_text}>Войти</span>
-      </div>
-    </Link>
   );
 }
