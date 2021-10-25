fe367026041fdf4f1f03c67bc7bc989f15eb2534
DemonRed <demonred8@gmail.com>
2021-10-25 16:37:03 +0300

    refactor PopUpModule

--- client/src/Components/PopUpModule/PopUpModule.js
+++ client/src/Components/PopUpModule/PopUpModule.js
@@ -1,11 +1,18 @@
 import cl from './PopUpModule.module.css';
 import cn from 'classnames';
 
-export default function PopUpModule({ children, visible }) {
+export default function PopUpModule ({
+  children,
+  visible,
+  hovered,
+}) {
   return (
-    <div className={cn(cl.popup, { [cl.active]: visible })}>
-      <div className={cn(cl.popup_container, { [cl.active]: visible })} />
-      <div className={cl.popup_children} onClick={(e) => e.stopPropagation()}>
+    <div
+      className={cn(cl.popup, { [cl.active]: visible || hovered })}
+    >
+      <div
+        className={cl.popup_children}
+      >
         {children}
       </div>
     </div>
