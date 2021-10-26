7f14b131e82092f3acbd0f5561764c6977d5c65d
Ivan Khomenko <simkav2411@gmail.com>
2021-10-26 10:40:08 +0300

    refactor

--- server/src/pizzas/multer-config.ts
+++ server/src/pizzas/multer-config.ts
@@ -10,31 +10,9 @@ export const storage = diskStorage({
   },
 });
 
-export const fileFilter = (_, file: FileFromMulter, cb) => {
+export const fileFilter = (_, file: Express.Multer.File, cb) => {
   file.mimetype.includes('image')
     ? cb(null, true)
     : cb(new InvalidFileType(), false);
 };
 export const limits = { fileSize: 20971520 };
-
-type FileFromMulter = {
-  // TODO refactor to types
-  /** Field name specified in the form */
-  fieldname: string;
-  /** Name of the file on the user's computer */
-  originalname: string;
-  /** Encoding type of the file */
-  encoding: string;
-  /** Mime type of the file */
-  mimetype: string;
-  /** Size of the file in bytes */
-  size: number;
-  /** The folder to which the file has been saved (DiskStorage) */
-  destination: string;
-  /** The name of the file within the destination (DiskStorage) */
-  filename: string;
-  /** Location of the uploaded file (DiskStorage) */
-  path: string;
-  /** A Buffer of the entire file (MemoryStorage) */
-  buffer: Buffer;
-};
