c115e9a7e55daac2778e6d42ee4f66361de276db
DemonRed <demonred8@gmail.com>
2021-10-29 14:52:33 +0300

    add memo

--- client/src/Components/LoadSpinner/LoadSpinner.js
+++ client/src/Components/LoadSpinner/LoadSpinner.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { css } from '@emotion/react';
 import GridLoader from 'react-spinners/GridLoader';
 import cl from './LoadSpinner.module.css';
 
-export default function LoadSpinner() {
+export default memo(function LoadSpinner () {
   const override = css`
     display: block;
     margin: 0 0;
@@ -15,4 +15,4 @@ export default function LoadSpinner() {
       <GridLoader color={'teal'} css={override} size={100} />
     </div>
   );
-}
+});
