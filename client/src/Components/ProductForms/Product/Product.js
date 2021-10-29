7372bd52c617bc68643f906a63bd8abc83611ef3
DemonRed <demonred8@gmail.com>
2021-10-29 14:51:06 +0300

    refactor Product component

--- client/src/Components/ProductForms/Product/Product.js
+++ client/src/Components/ProductForms/Product/Product.js
@@ -2,23 +2,16 @@ import cl from './Product.module.css';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductIngridients from '../ProductIngridients/ProductIngridients';
 import ProductFooter from '../ProductFooter/ProductFooter';
-import useHovered from '../../../Hooks/useHovered';
 
-export default function Product ({ setIsOrdered, item, ingridients }) {
-  const [isHovered, hoverRef] = useHovered();
-  
+export default function Product ({ item, ingridients }) {
   return (
     <li className={cl.product}>
       {
-        <div ref={hoverRef} className={cl.product_link_container}>
+        <div className={cl.product_link_container}>
           <ProductImage item={item} />
           <h3>{item.name}</h3>
           <ProductIngridients item={item} ingridients={ingridients} />
-          <ProductFooter
-            isHovered={isHovered}
-            setIsOrdered={setIsOrdered}
-            item={item}
-          />
+          <ProductFooter item={item} />
         </div>
       }
     </li>
