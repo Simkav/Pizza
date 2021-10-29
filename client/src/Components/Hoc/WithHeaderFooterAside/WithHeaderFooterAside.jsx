dae9e06120c68855b361e2cfc92e389657f1eced
DemonRed <demonred8@gmail.com>
2021-10-29 14:52:51 +0300

    refactor WithHeaderFooterAside hoc

--- client/src/Components/Hoc/WithHeaderFooterAside/WithHeaderFooterAside.jsx
+++ client/src/Components/Hoc/WithHeaderFooterAside/WithHeaderFooterAside.jsx
@@ -1,19 +1,28 @@
-import React from "react";
-import Footer from "../../Footer/Footer";
-import Aside from "../../Aside/Aside";
-import cl from './WithHeaderFooterAside.module.css'
-import Header from "../../HeaderForms/Header/Header";
-const WithHeaderFooterAside = (Component) => (props) => {
-  return (
-    <>
-      <Header />
-      <Aside />
-      <div className={cl.content_container}>
-        <Component />
-      </div>
-      <Footer />
-    </>
-  );
-};
+import Footer from '../../Footer/Footer';
+import Aside from '../../AsideForms/Aside/Aside';
+import cl from './WithHeaderFooterAside.module.css';
+import Header from '../../HeaderForms/Header/Header';
+import { useState } from 'react';
 
+const WithHeaderFooterAside = (Component) => {
+  return function Comp (props) {
+    const [hamburgerMenuState, setHamburgerMenuState] = useState(false);
+    return (
+      <>
+        <Header
+          hamburgerMenuState={hamburgerMenuState}
+          setHamburgerMenuState={setHamburgerMenuState}
+        />
+        <Aside
+          hamburgerMenuState={hamburgerMenuState}
+          setHamburgerMenuState={setHamburgerMenuState}
+        />
+        <div className={cl.content_container}>
+          <Component />
+        </div>
+        <Footer />
+      </>
+    );
+  };
+};
 export default WithHeaderFooterAside;
