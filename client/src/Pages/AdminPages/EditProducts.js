77ea1bba312302fad02eb8ba19d250781f30a717
DemonRed <demonred8@gmail.com>
2021-10-29 15:02:54 +0300

    fix deps path

--- client/src/Pages/AdminPages/EditProducts.js
+++ client/src/Pages/AdminPages/EditProducts.js
@@ -1,4 +1,4 @@
-import AdminPageHeader from '../../Components/AdminPageHeader/AdminPageHeader';
+import AdminPageHeader from '../../Components/AdminPageForms/AdminPageHeader/AdminPageHeader';
 import EditProductsForm from '../../Components/EditProductsForms/EditProductsForm/EditProductsForm';
 
 export default function EditProducts() {
