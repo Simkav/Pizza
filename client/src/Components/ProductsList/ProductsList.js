514279fae44684dc8c18f003a4dcfc6832d9a7ed
DemonRed <demonred8@gmail.com>
2021-10-29 14:48:27 +0300

    refactor ProductsList component

--- client/src/Components/ProductsList/ProductsList.js
+++ client/src/Components/ProductsList/ProductsList.js
@@ -1,25 +1,12 @@
-import { useState } from 'react';
 import Product from '../ProductForms/Product/Product';
-import ProductOrderModal from '../ProductForms/ProductOrderModal/ProductOrderModal';
 import cl from './ProductsList.module.css';
 
-export default function ProductsList({ products, ingridients }) {
-  const [isOrdered, setIsOrdered] = useState(false);
+export default function ProductsList ({ products, ingridients }) {
   return (
-    <>
-      <ul className={cl.products_container}>
-        {products.map((item) => {
-          return (
-            <Product
-              setIsOrdered={setIsOrdered}
-              key={item.id}
-              item={item}
-              ingridients={ingridients}
-            />
-          );
-        })}
-      </ul>
-      {<ProductOrderModal isOrdered={isOrdered} setIsOrdered={setIsOrdered} />}
-    </>
+    <ul className={cl.products_container}>
+      {products.map((item) => {
+        return <Product key={item.id} item={item} ingridients={ingridients} />;
+      })}
+    </ul>
   );
 }
