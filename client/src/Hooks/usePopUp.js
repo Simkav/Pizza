5d5aa1efdba31ad8006d8273a80538526115e424
DemonRed <demonred8@gmail.com>
2021-10-29 14:45:46 +0300

    refactor usePopUp hook

--- client/src/Hooks/usePopUp.js
+++ client/src/Hooks/usePopUp.js
@@ -3,35 +3,52 @@ import { useState, useRef, useCallback } from 'react';
 export default function usePopUp () {
   const [value, setValue] = useState({
     hovered: false,
-    visible: false
+    visible: false,
   });
 
-  const handleMouseOver = useCallback(() => setValue((value) => ({...value, hovered : true})), []);
-  const handleMouseOut = useCallback(() => setValue((value) => ({...value, hovered : false})), []);
+  const handleMouseOver = useCallback(
+    () => setValue((value) => ({ ...value, hovered: true })),
+    [],
+  );
+  const handleMouseOut = useCallback(
+    () => setValue((value) => ({ ...value, hovered: false })),
+    [],
+  );
 
-  const handleClick = useCallback(() => setValue((value) => ({...value, visible : true})), []);
-  const handleBlur = useCallback(() => setValue((value) => ({...value, visible : false})), []);
+  const handleClick = useCallback(
+    () => setValue((value) => ({ ...value, visible: true })),
+    [],
+  );
+  const handleBlur = useCallback(
+    () => setValue((value) => ({ ...value, visible: false })),
+    [],
+  );
+
+  const usePopUpItems = Object.entries({
+    mouseenter: handleMouseOver,
+    mouseleave: handleMouseOut,
+    click: handleClick,
+    blur: handleBlur,
+  });
 
   const ref = useRef();
 
   const callbackRef = useCallback(
     (node) => {
       if (ref.current) {
-        ref.current.removeAttribute('tabIndex')
-        ref.current.removeEventListener('mouseenter', handleMouseOver);
-        ref.current.removeEventListener('mouseleave', handleMouseOut);
-        ref.current.removeEventListener('click', handleClick);
-        ref.current.removeEventListener('blur', handleBlur);
+        usePopUpItems.forEach(([name, fn]) =>
+          ref.current.removeEventListener(name, fn),
+        );
+        ref.current.removeAttribute('tabIndex');
       }
 
       ref.current = node;
 
       if (ref.current) {
-        ref.current.setAttribute('tabIndex', -1)
-        ref.current.addEventListener('mouseenter', handleMouseOver);
-        ref.current.addEventListener('mouseleave', handleMouseOut);
-        ref.current.addEventListener('click', handleClick);
-        ref.current.addEventListener('blur', handleBlur);
+        usePopUpItems.forEach(([name, fn]) =>
+          ref.current.addEventListener(name, fn),
+        );
+        ref.current.setAttribute('tabIndex', -1);
       }
     },
     [handleMouseOver, handleMouseOut, handleClick, handleBlur],
