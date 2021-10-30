877f4e8e47df4b5067b4adb64fbf29b1af2ec983
Ivan Khomenko <simkav2411@gmail.com>
2021-10-30 10:14:31 +0300

    refactor

--- server/src/pizzas/dto/create-pizza.dto.ts
+++ server/src/pizzas/dto/create-pizza.dto.ts
@@ -1,7 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNumber } from 'class-validator';
 export class CreatePizzaDto {
-  @IsString()
   @ApiProperty({ description: 'Name of created pizza', example: 'Pizza Yolo' })
   name: string;
   @ApiProperty({
@@ -9,20 +7,16 @@ export class CreatePizzaDto {
     description: 'Any image type, with 20mb threshold',
   })
   image: any;
-  // THERE MUST BE IsNumber but swagger govno
   @ApiProperty({
     description: 'Weight of created pizza',
     example: 300,
   })
   weight: number;
-  // THERE MUST BE IsNumber but swagger govno
-
   @ApiProperty({
     description: 'Price of created pizza',
     example: 300,
   })
   price: number;
-  @IsString()
   @ApiProperty({
     example: '[1,2,3,4,5,6]',
     description: 'array of numbers in string',
