1defddfb640bda923d2bb44fc08a77c28556c660
DemonRed <demonred8@gmail.com>
2021-10-30 12:11:56 +0300

    refactor

--- client/src/Components/HeaderForms/Header/Header.js
+++ client/src/Components/HeaderForms/Header/Header.js
@@ -1,9 +1,8 @@
 import cl from './Header.module.css';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import cn from 'classnames';
 import HeaderNavigation from '../HeaderNavigation/HeaderNavigation';
 import { memo } from 'react';
 import HeaderSiteLogo from '../HeaderSiteLogo/HeaderSiteLogo';
+import HeaderHamburgerMenu from '../HeaderHamburgerMenu/HeaderHamburgerMenu';
 
 export default memo(function Header ({
   hamburgerMenuState,
@@ -11,20 +10,10 @@ export default memo(function Header ({
 }) {
   return (
     <header className={cl.header}>
-      <div
-        className={cn(cl.hamburger_menu_container, cl.hover_element, {
-          [cl.hamburger_menu_active]: hamburgerMenuState,
-        })}
-        onClick={() =>
-          setHamburgerMenuState((hamburgerMenuState) => !hamburgerMenuState)
-        }
-      >
-        {!hamburgerMenuState ? (
-          <FaBars className={cl.hamburger_bars} />
-        ) : (
-          <FaTimes className={cl.hamburger_cross} />
-        )}
-      </div>
+      <HeaderHamburgerMenu
+        hamburgerMenuState={hamburgerMenuState}
+        setHamburgerMenuState={setHamburgerMenuState}
+      />
       <HeaderSiteLogo />
       <HeaderNavigation />
     </header>
